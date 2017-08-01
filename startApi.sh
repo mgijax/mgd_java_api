152c4cfd9dc0d7e40b93ff03b879d6a0db9239d0
pf <peter.frost@jax.org>
2017-08-01 11:26:09 -0400

    Adding full path to java call.  Needs Java8.  Setup to run on scrumdogdev

--- startApi.sh
+++ startApi.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -jar target/mgd_java_api-swarm.jar -Papp.properties & 
+
+# setup for scrumdogdev server, called via Jenkins
+/usr/java/jdk1.8.0_131/bin/java -jar target/mgd_java_api-swarm.jar -Papp.properties &

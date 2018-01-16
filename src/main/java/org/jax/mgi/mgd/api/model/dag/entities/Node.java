185fb25be477425f920fe450d1515ddb0adc4c02
Sharon Giannatto <sharon.giannatto@jax.org>
2018-01-16 11:58:14 -0500

    created with just primary key - it is a complex B join table - in order to reference in Edge

---
+++ src/main/java/org/jax/mgi/mgd/api/model/dag/entities/Node.java
@@ -0,0 +1,29 @@
+package org.jax.mgi.mgd.api.model.dag.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.jax.mgi.mgd.api.model.EntityBase;
+import org.jax.mgi.mgd.api.model.bib.entities.Reference;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@ApiModel(value = "DAG Node Model Object")
+@Table(name="dag_node")
+public class Node extends EntityBase {
+
+	@Id
+	private Integer _node_key;
+	
+	
+}

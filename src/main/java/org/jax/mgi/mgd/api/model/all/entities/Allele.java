90950b0bd60fd4c97cba749acedf87097d3207bf
Sharon Giannatto <sharon.giannatto@jax.org>
2018-01-16 11:01:52 -0500

    added 1-1 relationships

--- src/main/java/org/jax/mgi/mgd/api/model/all/entities/Allele.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/entities/Allele.java
@@ -3,10 +3,20 @@ package org.jax.mgi.mgd.api.model.all.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.jax.mgi.mgd.api.model.EntityBase;
+import org.jax.mgi.mgd.api.model.mgi.entities.Organism;
+import org.jax.mgi.mgd.api.model.mgi.entities.User;
+import org.jax.mgi.mgd.api.model.mrk.entities.Marker;
+import org.jax.mgi.mgd.api.model.prb.entities.Strain;
+import org.jax.mgi.mgd.api.model.voc.entities.Term;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
@@ -28,4 +38,59 @@ public class Allele extends EntityBase {
 	private Date approval_date;
 	private Date creation_date;
 	private Date modification_date;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_marker_key", referencedColumnName="_marker_key")
+	private Marker marker;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_strain_key", referencedColumnName="_strain_key")
+	private Strain strain;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_mode_key", referencedColumnName="_term_key")
+	private Term inheritanceMode;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_allele_type_key", referencedColumnName="_term_key")
+	private Term alleleType;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_allele_status_key", referencedColumnName="_term_key")
+	private Term alleleStatus;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_transmission_key", referencedColumnName="_term_key")
+	private Term transmission;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_collection_key", referencedColumnName="_term_key")
+	private Term collection;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_markerallele_status_key", referencedColumnName="_term_key")
+	private Term markerAlleleStatus;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_createdby_key", referencedColumnName="_user_key")
+	private User createdBy;
+
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_modifiedby_key", referencedColumnName="_user_key")
+	private User modifiedBy;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_approvedby_key", referencedColumnName="_user_key")
+	private User approvedBy;
 }

171bed7da6e68a4f5c71d0dc73870875f611fe7a
Lori Corbani <lori.corbani@jax.org>
2018-01-19 07:37:16 -0500

    TR12662/AllelePair

---
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/entities/AllelePair.java
@@ -0,0 +1,86 @@
+package org.jax.mgi.mgd.api.model.gxd.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.jax.mgi.mgd.api.model.EntityBase;
+import org.jax.mgi.mgd.api.model.all.entities.Allele;
+import org.jax.mgi.mgd.api.model.all.entities.CellLine;
+import org.jax.mgi.mgd.api.model.mgi.entities.User;
+import org.jax.mgi.mgd.api.model.mrk.entities.Marker;
+import org.jax.mgi.mgd.api.model.voc.entities.Term;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@ApiModel(value = "AllelePair Model Object")
+@Table(name="gxd_allelepair")
+public class AllelePair extends EntityBase {
+
+	@Id
+	private Integer _allelepair_key;
+	private Integer sequenceNum;
+	private Date creation_date;
+	private Date modification_date;
+	
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_genotype_key", referencedColumnName="_genotype_key")
+	private Genotype genotype;
+
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_allele_key_1", referencedColumnName="_allele_key")
+	private Allele allele1;
+
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_allele_key_2", referencedColumnName="_allele_key")
+	private Allele allele2;
+
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_marker_key", referencedColumnName="_marker_key")
+	private Marker marker;
+
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_mutantcellline_key_1", referencedColumnName="_cellline_key")
+	private CellLine cellLine1;
+
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_mutantcellline_key_2", referencedColumnName="_cellline_key")
+	private CellLine cellLine2;
+
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_pairstate_key", referencedColumnName="_term_key")
+	private Term pairState;
+
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_compound_key", referencedColumnName="_term_key")
+	private Term compound;
+
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_createdby_key", referencedColumnName="_user_key")
+	private User createdBy;
+	
+	@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_modifiedby_key", referencedColumnName="_user_key")
+	private User modifiedBy;
+}

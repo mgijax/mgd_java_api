8270dc1512b9be2fcc86abd4ed1e3d11a9141d3a
Lori Corbani <lori.corbani@jax.org>
2018-01-24 12:46:49 -0500

    TR12262/MGIProperty

---
+++ src/main/java/org/jax/mgi/mgd/api/rest/interfaces/test/MGIPropertyRESTInterface.java
@@ -0,0 +1,20 @@
+package org.jax.mgi.mgd.api.rest.interfaces.test;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jax.mgi.mgd.api.model.mgi.entities.MGIProperty;
+import org.jax.mgi.mgd.api.rest.interfaces.RESTInterface;
+
+import io.swagger.annotations.Api;
+
+@Path("/mgiproperty")
+@Api(value = "MGI Property Endpoints")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public interface MGIPropertyRESTInterface extends RESTInterface<MGIProperty> {
+
+
+}

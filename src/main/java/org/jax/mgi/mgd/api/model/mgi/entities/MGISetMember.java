e80ee6835010b72c0f9a25bf4c6679820db9b833
Olin Blodgett <nilo@nilosplace.net>
2018-01-25 12:51:36 -0500

    Added mgi set

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/MGISetMember.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/MGISetMember.java
@@ -1,18 +1,15 @@
 package org.jax.mgi.mgd.api.model.mgi.entities;
 
 import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.jax.mgi.mgd.api.model.EntityBase;
-import org.jax.mgi.mgd.api.model.acc.entities.MGIType;
 
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
@@ -20,20 +17,16 @@ import lombok.Setter;
 
 @Getter @Setter
 @Entity
-@ApiModel(value = "MGISet Object")
-@Table(name="mgi_set")
-public class MGISet extends EntityBase {
+@ApiModel(value = "MGISet Member Object")
+@Table(name="mgi_setmember")
+public class MGISetMember extends EntityBase {
 	@Id
-	private Integer _set_key;
-	private String name;
+	private Integer _setmember_key;
+	
+	private String label;
 	private Integer sequenceNum;
 	private Date creation_date;
 	private Date modification_date;
-	
-	//@JsonIgnore
-	@OneToOne(fetch=FetchType.EAGER)
-	@JoinColumn(name="_mgitype_key", referencedColumnName="_mgitype_key")
-	private MGIType mgiType;
 	
 	//@JsonIgnore
 	@OneToOne(fetch=FetchType.EAGER)
@@ -45,8 +38,8 @@ public class MGISet extends EntityBase {
 	@JoinColumn(name="_modifiedby_key", referencedColumnName="_user_key")
 	private User modifiedBy;
 	
-	@OneToMany(fetch=FetchType.EAGER)
+	@OneToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name="_set_key")
-	private Set<MGISetMember> setMembers;
+	private MGISet mgiSet;
 
 }

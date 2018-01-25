489234d78a122bdd18ee45fdbb63fb0f02c440e5
Lori Corbani <lori.corbani@jax.org>
2018-01-25 11:16:33 -0500

    TR12262/add Annotation/AnnotType joins

---
+++ src/main/java/org/jax/mgi/mgd/api/model/voc/entities/Annotation.java
@@ -0,0 +1,44 @@
+package org.jax.mgi.mgd.api.model.voc.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.jax.mgi.mgd.api.model.EntityBase;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@ApiModel(value = "Annotation Model Object")
+@Table(name="voc_annot")
+public class Annotation extends EntityBase {
+
+	@Id
+	private Integer _annot_key;
+	private Date creation_date;
+	private Date modification_date;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_annottype_key", referencedColumnName="_annottype_key")
+	private AnnotType annotType;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_term_key", referencedColumnName="_term_key")
+	private Term term;
+	
+	//@JsonIgnore
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="_qualifier_key", referencedColumnName="_term_key")
+	private Term qualifier;
+	
+}

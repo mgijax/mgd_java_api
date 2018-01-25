b9578a85574566538319ccce96b13403a90dc236
Lori Corbani <lori.corbani@jax.org>
2018-01-25 12:37:07 -0500

    TR12262/add Annotation/AnnotType joins

--- src/main/java/org/jax/mgi/mgd/api/model/voc/entities/AnnotHeader.java
+++ src/main/java/org/jax/mgi/mgd/api/model/voc/entities/AnnotHeader.java
@@ -1,11 +1,13 @@
 package org.jax.mgi.mgd.api.model.voc.entities;
 
 import java.util.Date;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -56,4 +58,8 @@ public class AnnotHeader extends EntityBase {
 	@OneToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name="_approvedby_key", referencedColumnName="_user_key")
 	private User approvedBy;
+	
+	@OneToMany(fetch=FetchType.EAGER)
+	@JoinColumn(name="_anottype_key", referencedColumnName="_annottype_key")
+	private Set<Annotation> annotations;
 }

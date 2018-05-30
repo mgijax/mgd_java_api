010c24a80116b2de5961c54b8729ec4d25226938
Olin Blodgett <nilo@nilosplace.net>
2018-05-30 13:46:42 -0400

    Added swagger documentation and re organized objects

--- src/main/java/org/jax/mgi/mgd/api/model/BaseSearchForm.java
+++ src/main/java/org/jax/mgi/mgd/api/model/BaseSearchForm.java
@@ -2,17 +2,25 @@ package org.jax.mgi.mgd.api.model;
 
 import java.util.Map;
 
+import org.jax.mgi.mgd.api.model.voc.search.TermSearchForm;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @ApiModel(value="Base Search Form", subTypes= {TermSearchForm.class})
 public abstract class BaseSearchForm {
 
+	@ApiModelProperty(hidden=true)
 	protected String orderBy;
+	@ApiModelProperty(hidden=true)
 	protected Integer pageNum;
+	@ApiModelProperty(hidden=true)
 	protected Integer pageSize;
 	
 	// <Database Field, Value to Search for>
+	@ApiModelProperty(hidden=true)
 	public abstract Map<String, Object> getSearchFields();
 	
 }
--- src/main/java/org/jax/mgi/mgd/api/model/voc/search/TermSearchForm.java
+++ src/main/java/org/jax/mgi/mgd/api/model/voc/search/TermSearchForm.java
@@ -5,13 +5,17 @@ import java.util.Map;
 
 import org.jax.mgi.mgd.api.model.BaseSearchForm;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @ApiModel("Term Search Form")
 public class TermSearchForm extends BaseSearchForm {
-
+	
+	@ApiModelProperty(value="Vocab Name to Search for")
 	private String vocabName;
+	@ApiModelProperty(value="Search for Term by term")
 	private String term;
 	
 	@Override

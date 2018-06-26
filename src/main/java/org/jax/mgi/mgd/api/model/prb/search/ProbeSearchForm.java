547f080a506a8aa2d83f721377406a79ae43b745
Lori Corbani <lori.corbani@jax.org>
2018-06-26 09:23:22 -0400

    TR12734/remove unused mgiAccession from searches/broken anyway

--- src/main/java/org/jax/mgi/mgd/api/model/prb/search/ProbeSearchForm.java
+++ src/main/java/org/jax/mgi/mgd/api/model/prb/search/ProbeSearchForm.java
@@ -1,26 +1,19 @@
 package org.jax.mgi.mgd.api.model.prb.search;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.jax.mgi.mgd.api.model.BaseSearchForm;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter @ApiModel("Probe Search Form")
 public class ProbeSearchForm extends BaseSearchForm {
 
-	@ApiModelProperty(value="Accession Id to Search for")
-	private String accid;
-	
 	@Override
 	public Map<String, Object> getSearchFields() {
-		Map<String, Object> ret = new HashMap<>();
-		if(accid != null) { ret.put("mgiAccessionId.accID", accid); }
-		return ret;
+		return null;
 	}
 	
 }

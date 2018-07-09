d622777c524484378f6f1b9f37095b454c2c9b92
Lori Corbani <lori.corbani@jax.org>
2018-07-09 12:09:21 -0400

    TR12734/add Accession Endpoint

--- src/main/java/org/jax/mgi/mgd/api/model/acc/search/AccessionSearchForm.java
+++ src/main/java/org/jax/mgi/mgd/api/model/acc/search/AccessionSearchForm.java
@@ -15,11 +15,15 @@ public class AccessionSearchForm extends BaseSearchForm {
 
 	@ApiModelProperty(value="Accession Id to Search for")
 	private String accid;
+	private Integer _logicaldb_key;
+	private Integer _mgitype_key;
 	
 	@Override
 	public Map<String, Object> getSearchFields() {
 		Map<String, Object> ret = new HashMap<>();
 		if(accid != null) { ret.put("accID", accid); }
+		if(_logicaldb_key != null) { ret.put("_logicaldb_key", _logicaldb_key); }
+		if(_mgitype_key != null) { ret.put("_mgitype_key", _mgitype_key); }
 		return ret;
 	}
 	

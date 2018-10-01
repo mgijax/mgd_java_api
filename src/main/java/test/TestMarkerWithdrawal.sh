0eac91773dc09f99020a2a4dc916eac3b31a3f88
Lori Corbani <lori.corbani@jax.org>
2018-10-01 14:13:04 -0400

    TR12964/TestMarkerWithdrawal

--- src/main/java/test/TestMarkerWithdrawal.sh
+++ src/main/java/test/TestMarkerWithdrawal.sh
@@ -11,7 +11,7 @@ export CONFIG_MASTER
 . ${CONFIG_MASTER}
 echo $MGICONFIG
 
-CLASSPATH=/home/lec/mgi/mgd_java_api/target/classes:/home/lec/mgi/mgd_java_api/src/lib/core.jar:${COMMON_CLASSPATH}
+CLASSPATH=${MGD_JAVA_API}/target/classes:${MGD_JAVA_API}/src/lib/core.jar:${COMMON_CLASSPATH}
 export CLASSPATH
 echo $CLASSPATH
 

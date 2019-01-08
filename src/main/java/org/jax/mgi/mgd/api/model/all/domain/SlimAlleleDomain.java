274d26c5af7a07fabfbce8f97509af1aa41a18fa
Lori Corbani <lori.corbani@jax.org>
2019-01-08 14:58:28 -0500

    TR12963/teleuse-to-mgd_java_api

--- src/main/java/org/jax/mgi/mgd/api/model/all/domain/SlimAlleleDomain.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/domain/SlimAlleleDomain.java
@@ -3,7 +3,7 @@ package org.jax.mgi.mgd.api.model.all.domain;
 import java.util.List;
 
 import org.jax.mgi.mgd.api.model.BaseDomain;
-import org.jax.mgi.mgd.api.model.acc.domain.AccessionDomain;
+import org.jax.mgi.mgd.api.model.acc.domain.SlimAccessionDomain;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,5 +13,5 @@ public class SlimAlleleDomain extends BaseDomain {
 
 	private String alleleKey;
 	private String symbol;       
-	private List<AccessionDomain> mgiAccessionIds;
+	private List<SlimAccessionDomain> mgiAccessionIds;
 }
--- src/main/java/org/jax/mgi/mgd/api/model/all/translator/SlimAlleleTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/translator/SlimAlleleTranslator.java
@@ -2,14 +2,14 @@ package org.jax.mgi.mgd.api.model.all.translator;
 
 import org.apache.commons.collections4.IteratorUtils;
 import org.jax.mgi.mgd.api.model.BaseEntityDomainTranslator;
-import org.jax.mgi.mgd.api.model.acc.domain.AccessionDomain;
-import org.jax.mgi.mgd.api.model.acc.translator.AccessionTranslator;
+import org.jax.mgi.mgd.api.model.acc.domain.SlimAccessionDomain;
+import org.jax.mgi.mgd.api.model.acc.translator.SlimAccessionTranslator;
 import org.jax.mgi.mgd.api.model.all.domain.SlimAlleleDomain;
 import org.jax.mgi.mgd.api.model.all.entities.Allele;
 
 public class SlimAlleleTranslator extends BaseEntityDomainTranslator<Allele, SlimAlleleDomain> {
 
-	private AccessionTranslator accessionTranslator = new AccessionTranslator();
+	private SlimAccessionTranslator accessionTranslator = new SlimAccessionTranslator();
 	
 	@Override
 	protected SlimAlleleDomain entityToDomain(Allele entity, int translationDepth) {
@@ -21,7 +21,7 @@ public class SlimAlleleTranslator extends BaseEntityDomainTranslator<Allele, Sli
 
 		// mgi accession ids only
 		if (entity.getMgiAccessionIds() != null) {
-			Iterable<AccessionDomain> acc = accessionTranslator.translateEntities(entity.getMgiAccessionIds());
+			Iterable<SlimAccessionDomain> acc = accessionTranslator.translateEntities(entity.getMgiAccessionIds());
 			if(acc.iterator().hasNext() == true) {
 				domain.setMgiAccessionIds(IteratorUtils.toList(acc.iterator()));
 			}

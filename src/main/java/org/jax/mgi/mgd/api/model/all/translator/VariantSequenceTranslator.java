bc98cf4500a118bc3e01b1020c524357513d7fe0
Sharon Giannatto <sharon.giannatto@jax.org>
2019-03-07 07:48:57 -0500

    changes accessionID list test from Size to isEmpty

--- src/main/java/org/jax/mgi/mgd/api/model/all/translator/VariantSequenceTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/translator/VariantSequenceTranslator.java
@@ -34,10 +34,7 @@ public class VariantSequenceTranslator extends BaseEntityDomainTranslator<Varian
 		domain.setModification_date(dateFormatNoTime.format(entity.getModification_date()));
 
 		// variant sequence accession ids
-		System.out.println("VSTranslator entity: " + entity.getAccessionIds());
-       // if ((entity.getAccessionIds() != null) && (entity.getAccessionIds().size() > 0)) {	
-		if (entity.getAccessionIds().size() > 0) {
-        	System.out.println("VSTranslator entity: " + entity.getAccessionIds());
+		if (!entity.getAccessionIds().isEmpty()) {
 			Iterable<AccessionDomain> acc = accessionTranslator.translateEntities(entity.getAccessionIds());
 			if(acc.iterator().hasNext() == true) {
 				domain.setAccessionIds(IteratorUtils.toList(acc.iterator()));

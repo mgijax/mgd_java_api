fec58963ef64a2ea19e2ab516fb6defec56685c5
Lori Corbani <lori.corbani@jax.org>
2019-04-18 14:14:59 -0400

    TR12963/teleuse-to-mgd_java_api

--- src/main/java/org/jax/mgi/mgd/api/model/mrk/service/MarkerService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mrk/service/MarkerService.java
@@ -122,7 +122,8 @@ public class MarkerService extends BaseService<MarkerDomain> {
 		
 		// default reference is J:23000 (22864)
 		String refKey;
-		if (domain.getHistory() == null || domain.getHistory().isEmpty()) {
+		//if (domain.getHistory() == null || domain.getHistory().isEmpty()) {
+		if (domain.getHistory().get(0).getRefsKey() != null && domain.getHistory().get(0).getRefsKey().isEmpty()     ) {
 			refKey = "22864";
 		}
 		else {

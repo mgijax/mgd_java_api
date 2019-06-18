7ba5989c1ab03836ac4ff71ad4d870dd2b033828
Lori Corbani <lori.corbani@jax.org>
2019-06-18 08:10:42 -0400

    TR13007/RNA Sequence project

--- src/main/java/org/jax/mgi/mgd/api/model/img/service/ImageService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/img/service/ImageService.java
@@ -151,10 +151,12 @@ public class ImageService extends BaseService<ImageDomain> {
 		log.info("processImage/update");
 
 		// copyright/DXDOI validation
-		if (domain.getCopyrightNote().getNoteChunk().contains("DXDOI(||)")) {
-			log.info("processImage/update/DXDOI missing");
-			results.setError("Failed : Copyright/DXDOI error", "Copyright is missing the DXDOI identifier", Constants.HTTP_SERVER_ERROR);
-			return results;		
+		if (domain.getCopyrightNote() != null) {
+			if (domain.getCopyrightNote().getNoteChunk().contains("DXDOI(||)")) {
+				log.info("processImage/update/DXDOI missing");
+				results.setError("Failed : Copyright/DXDOI error", "Copyright is missing the DXDOI identifier", Constants.HTTP_SERVER_ERROR);
+				return results;	
+			}
 		}
 		
 		if (!String.valueOf(entity.getReference().get_refs_key()).equals(domain.getRefsKey())) {

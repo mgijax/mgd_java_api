88b79d02609746a208d085141fb5262aa7314081
Lori Corbani <lori.corbani@jax.org>
2019-07-17 08:44:13 -0400

    TR13007/RNA Sequence project

--- src/main/java/org/jax/mgi/mgd/api/model/img/translator/ImageSubmissionTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/img/translator/ImageSubmissionTranslator.java
@@ -16,20 +16,29 @@ public class ImageSubmissionTranslator extends BaseEntityDomainTranslator<Image,
 		ImageSubmissionDomain domain = new ImageSubmissionDomain();
 
 		domain.setProcessStatus(Constants.PROCESS_NOTDIRTY);
-		domain.setImageStatus("Image file already loaded.");
 		domain.setImageClassKey(String.valueOf(entity.getImageClass().get_term_key()));
 		domain.setImageClass(entity.getImageClass().getTerm());
 		domain.setImageKey(String.valueOf(entity.get_image_key()));
 		domain.setImageTypeKey(String.valueOf(entity.getImageType().get_term_key()));
-		domain.setImageType(entity.getImageType().getTerm());
-		domain.setXDim(String.valueOf(entity.getXDim()));
-		domain.setYDim(String.valueOf(entity.getYDim()));		
+		domain.setImageType(entity.getImageType().getTerm());		
 		domain.setFigureLabel(entity.getFigureLabel());
 
 		// reference
 		domain.setRefsKey(String.valueOf(entity.getReference().get_refs_key()));
 		domain.setJnumid(entity.getReference().getReferenceCitationCache().getJnumid());
 		domain.setShort_citation(entity.getReference().getReferenceCitationCache().getShort_citation());
+
+		if (entity.getXDim() != null) {
+			domain.setXDim(String.valueOf(entity.getXDim()));
+			domain.setImageStatus("Image file already loaded.");
+		}
+		else {
+			domain.setImageStatus("No file chosen.");	
+		}
+		
+		if (entity.getYDim() != null) {
+			domain.setYDim(String.valueOf(entity.getYDim()));
+		}
 
 		if (entity.getThumbnailImage() != null) {
 			domain.setThumbnailFigureLabel(entity.getThumbnailImage().getFigureLabel());

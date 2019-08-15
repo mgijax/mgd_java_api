bd9d61a01f50146f3ffacb39d431ad97e6b3eed3
Lori Corbani <lori.corbani@jax.org>
2019-08-15 11:33:00 -0400

    TR13166/marker module/utilities/rename

--- src/main/java/org/jax/mgi/mgd/api/model/mrk/service/MarkerService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mrk/service/MarkerService.java
@@ -1091,7 +1091,8 @@ public class MarkerService extends BaseService<MarkerDomain> {
 
 		// mrk_event = rename
 		if (params.get("eventKey").equals("2")) {
-			runCmd = runCmd + " --newName='" + (String) params.get("newName") + "'";
+			runCmd = runCmd + " --newName='" + 
+						((String) params.get("newName")).replaceAll("'",  "''") + "'";
 			runCmd = runCmd + " --newSymbols='" + (String) params.get("newSymbol") + "'";
 		}
 		

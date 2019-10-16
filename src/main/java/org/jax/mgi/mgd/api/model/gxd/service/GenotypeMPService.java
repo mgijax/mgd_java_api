d75fb2d63a6069a5fd778d8db94929dd02d854f0
Lori Corbani <lori.corbani@jax.org>
2019-10-16 08:11:48 -0400

    TR13077/RNA Sequence project/MP Annot

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/service/GenotypeMPService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/service/GenotypeMPService.java
@@ -2,7 +2,6 @@ package org.jax.mgi.mgd.api.model.gxd.service;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 

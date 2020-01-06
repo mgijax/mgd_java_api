64ed7a9cfac47962a862c35c9b30482e671c3c25
Lori Corbani <lori.corbani@jax.org>
2020-01-06 06:31:51 -0500

    TR13007/RNA Sequence project/triage

--- src/main/java/org/jax/mgi/mgd/api/model/bib/service/ReferenceService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/bib/service/ReferenceService.java
@@ -22,6 +22,7 @@ import org.jax.mgi.mgd.api.model.bib.dao.ReferenceNoteDAO;
 import org.jax.mgi.mgd.api.model.bib.domain.ReferenceDomain;
 import org.jax.mgi.mgd.api.model.bib.domain.SlimReferenceDomain;
 import org.jax.mgi.mgd.api.model.bib.entities.LTReferenceWorkflowData;
+import org.jax.mgi.mgd.api.model.bib.entities.LTReferenceWorkflowStatus;
 import org.jax.mgi.mgd.api.model.bib.entities.Reference;
 import org.jax.mgi.mgd.api.model.bib.entities.ReferenceBook;
 import org.jax.mgi.mgd.api.model.bib.entities.ReferenceNote;
@@ -232,7 +233,7 @@ public class ReferenceService extends BaseService<ReferenceDomain> {
 		wfDataEntity.setModifiedByUser(user);
 		wfDataEntity.setModification_date(new Date());
 		wfDataDAO.persist(wfDataEntity);			
-
+		
 		// process pubmed accession ids
 		if (domain.getPubmedid() != null && !domain.getPubmedid().isEmpty()) {
 			AccessionDomain accessionDomain = new AccessionDomain();

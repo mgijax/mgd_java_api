698029327c28488b213126ec041b7cc566efe90e
Lori Corbani <lori.corbani@jax.org>
2020-03-23 10:03:37 -0400

    TR13204/Infrastructure/term

--- src/main/java/org/jax/mgi/mgd/api/model/voc/service/TermService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/voc/service/TermService.java
@@ -218,7 +218,7 @@ public class TermService extends BaseService<TermDomain> {
 				log.info("processTerm update");
 				Term entity = termDAO.get(Integer.valueOf(domain.get(i).getTermKey()));		
 
-				if (domain.get(i).getVocabKey() == "82") {
+				if (domain.get(i).getVocabKey().equals("82")) {
 					log.info("processTerm/update synonym");
 					modified = synonymService.process(String.valueOf(entity.get_term_key()), domain.get(i).getGoRelSynonyms(), mgiTypeKey, user);
 				}	

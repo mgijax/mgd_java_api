ea063c80fc43354748b8a0881b2d4af017fad2d6
Lori Corbani <lori.corbani@jax.org>
2020-03-27 12:07:20 -0400

    TR13204/Infrastructure/allele

--- src/main/java/org/jax/mgi/mgd/api/model/all/translator/AlleleTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/translator/AlleleTranslator.java
@@ -106,7 +106,7 @@ public class AlleleTranslator extends BaseEntityDomainTranslator<Allele, AlleleD
 			MGIReferenceAssocTranslator refAssocTranslator = new MGIReferenceAssocTranslator();
 			Iterable<MGIReferenceAssocDomain> i = refAssocTranslator.translateEntities(entity.getRefAssocs());
 			domain.setRefAssocs(IteratorUtils.toList(i.iterator()));
-			domain.getRefAssocs().sort(Comparator.comparingInt(MGIReferenceAssocDomain::getAllowOnlyOne).thenComparing(MGIReferenceAssocDomain::getRefAssocTypeKey));
+			domain.getRefAssocs().sort(Comparator.comparingInt(MGIReferenceAssocDomain::getAllowOnlyOne).reversed().thenComparing(MGIReferenceAssocDomain::getRefAssocTypeKey));
 		}	
 
 		// synonyms

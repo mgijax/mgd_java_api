5f06d441b42356beb91495596e3158b930417869
Lori Corbani <lori.corbani@jax.org>
2020-05-13 08:09:17 -0400

    TR13204/Infrastructure project

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/RelationshipAlleleDriverGene.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/RelationshipAlleleDriverGene.java
@@ -22,8 +22,8 @@ import lombok.Setter;
 public class RelationshipAlleleDriverGene extends BaseEntity {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mgi_relationship_drivergene_generator")
-	@SequenceGenerator(name="mgi_relationship_drivergene_generator", sequenceName = "mgi_relationship_seq", allocationSize=1)	
+//	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mgi_relationship_drivergene_generator")
+//	@SequenceGenerator(name="mgi_relationship_drivergene_generator", sequenceName = "mgi_relationship_seq", allocationSize=1)	
 	private int _relationship_key;
 	private int _allele_key;
 	private int _marker_key;

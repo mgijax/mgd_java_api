28f4ab80adced43627221834bf59ab1ded3338d7
Lori Corbani <lori.corbani@jax.org>
2020-05-13 07:29:34 -0400

    TR13204/Infrastructure project

--- src/main/java/org/jax/mgi/mgd/api/model/all/translator/AlleleTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/translator/AlleleTranslator.java
@@ -139,11 +139,11 @@ public class AlleleTranslator extends BaseEntityDomainTranslator<Allele, AlleleD
 		}
 	
 		// driver genes
-		if (entity.getDriverGenes() != null && !entity.getDriverGenes().isEmpty()) {
-			RelationshipAlleleDriverGeneTranslator driverTranslator = new RelationshipAlleleDriverGeneTranslator();
-			Iterable<RelationshipAlleleDriverGeneDomain> i = driverTranslator.translateEntities(entity.getDriverGenes());
-			domain.setDriverGenes(IteratorUtils.toList(i.iterator()));
-		}
+//		if (entity.getDriverGenes() != null && !entity.getDriverGenes().isEmpty()) {
+//			RelationshipAlleleDriverGeneTranslator driverTranslator = new RelationshipAlleleDriverGeneTranslator();
+//			Iterable<RelationshipAlleleDriverGeneDomain> i = driverTranslator.translateEntities(entity.getDriverGenes());
+//			domain.setDriverGenes(IteratorUtils.toList(i.iterator()));
+//		}
 
 		// imagepane associations by allele
 		if (entity.getImagePaneAssocs() != null && !entity.getImagePaneAssocs().isEmpty()) {

ea36e6f93a05de881122f5ecfd281f26c3845b16
Sharon Giannatto <sharon.giannatto@jax.org>
2020-06-02 08:20:03 -0400

    trying to  make source create method work with ageMinMax SP

---
+++ src/main/java/org/jax/mgi/mgd/api/model/prb/controller/ProbeSourceController.java
@@ -0,0 +1,61 @@
+package org.jax.mgi.mgd.api.model.prb.controller;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jax.mgi.mgd.api.model.BaseController;
+import org.jax.mgi.mgd.api.model.mgi.entities.User;
+import org.jax.mgi.mgd.api.model.prb.domain.ProbeSourceDomain;
+import org.jax.mgi.mgd.api.model.prb.service.ProbeSourceService;
+import org.jax.mgi.mgd.api.util.SearchResults;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Path("/source")
+@Api(value = "Source Endpoints")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProbeSourceController extends BaseController<ProbeSourceDomain> {
+
+	@Inject
+	private ProbeSourceService probeSourceService;
+
+	@POST
+	@ApiOperation(value = "run reset agemin/max sp for key")
+	@Path("/resetminmax")
+	public SearchResults<ProbeSourceDomain> runAgeMinMax(Integer key, User user) {
+		return probeSourceService.runAgeMinMax(key, user);
+	}
+	
+	@Override
+	public SearchResults<ProbeSourceDomain> create(ProbeSourceDomain domain, User user) {
+		SearchResults<ProbeSourceDomain> results = new SearchResults<ProbeSourceDomain>();
+		results = probeSourceService.create(domain, user);
+		results = probeSourceService.getResults(Integer.valueOf(results.items.get(0).getSourceKey()));
+		return results;
+	}
+
+	@Override
+	public SearchResults<ProbeSourceDomain> update(ProbeSourceDomain domain, User user) {
+		SearchResults<ProbeSourceDomain> results = new SearchResults<ProbeSourceDomain>();
+		results = probeSourceService.update(domain, user);
+		results = probeSourceService.getResults(Integer.valueOf(results.items.get(0).getSourceKey()));
+		return results;
+	}
+
+	@Override
+	public ProbeSourceDomain get(Integer key) {
+		return probeSourceService.get(key);
+	}
+
+	@Override
+	public SearchResults<ProbeSourceDomain> delete(Integer key, User user) {
+		return probeSourceService.delete(key, user);
+	}
+		
+}

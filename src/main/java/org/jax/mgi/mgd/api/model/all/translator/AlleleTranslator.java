f9ff27b4e1f593c19f7f8d62d62defd34833a0b1
Lori Corbani <lori.corbani@jax.org>
2020-06-05 09:40:17 -0400

    TR13204/Infrastructure project/Alleles

--- src/main/java/org/jax/mgi/mgd/api/model/all/translator/AlleleTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/translator/AlleleTranslator.java
@@ -65,9 +65,9 @@ public class AlleleTranslator extends BaseEntityDomainTranslator<Allele, AlleleD
 			domain.setApprovedBy(entity.getApprovedBy().getLogin());
 			domain.setApproval_date(dateFormatNoTime.format(entity.getApproval_date()));
 		}
-		
-		// primary mgi accession ids only
-		if (!entity.getMgiAccessionIds().isEmpty()) {
+
+		// mgi accession ids only
+		if (entity.getMgiAccessionIds() != null && !entity.getMgiAccessionIds().isEmpty()) {
 			domain.setAccID(entity.getMgiAccessionIds().get(0).getAccID());
 		}
 

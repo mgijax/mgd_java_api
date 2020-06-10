6d987afa72f24d03c2b2fc57abebfa873f69be62
Lori Corbani <lori.corbani@jax.org>
2020-06-10 09:25:29 -0400

    TR13204/Infrastructure project/Alleles

--- src/main/java/org/jax/mgi/mgd/api/model/mrk/translator/SlimMarkerTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mrk/translator/SlimMarkerTranslator.java
@@ -14,25 +14,17 @@ public class SlimMarkerTranslator extends BaseEntityDomainTranslator<Marker, Sli
 			
 		SlimMarkerDomain domain = new SlimMarkerDomain();
 		
-		log.info("marker key");
 		domain.setMarkerKey(String.valueOf(entity.get_marker_key()));
-		log.info("symbol");
 		domain.setSymbol(entity.getSymbol());
-		log.info("name");
 		domain.setName(entity.getName());
-		log.info("chr");
 		domain.setChromosome(entity.getChromosome());
-		log.info("organism key");
 		domain.setOrganismKey(String.valueOf(entity.getOrganism().get_organism_key()));
-		log.info("organism");
 		domain.setOrganism(entity.getOrganism().getCommonname());
-		log.info("latin");
 		domain.setOrganismLatin(entity.getOrganism().getLatinname());
-		log.info("modified by key");
 		domain.setModifiedByKey(entity.getModifiedBy().get_user_key().toString());
-		log.info("modified by");
 		domain.setModifiedBy(entity.getModifiedBy().getLogin());
 
+		log.info(entity.getMgiAccessionIds());
 //		if (entity.getMgiAccessionIds() != null) {
 //			domain.setAccID(entity.getMgiAccessionIds().get(0).getAccID());
 //		}

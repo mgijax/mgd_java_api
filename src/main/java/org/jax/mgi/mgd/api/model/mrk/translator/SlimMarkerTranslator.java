3e4a3184c598265bc8a46160fdd76dd6e6a495cc
Lori Corbani <lori.corbani@jax.org>
2020-06-10 09:36:16 -0400

    TR13204/Infrastructure project/Alleles

--- src/main/java/org/jax/mgi/mgd/api/model/mrk/translator/SlimMarkerTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mrk/translator/SlimMarkerTranslator.java
@@ -25,8 +25,7 @@ public class SlimMarkerTranslator extends BaseEntityDomainTranslator<Marker, Sli
 		domain.setModifiedBy(entity.getModifiedBy().getLogin());
 
 		log.info("getMgiAccessionIds: " + entity.getMgiAccessionIds());
-		log.info("getMgiAccessionIds: " + entity.getMgiAccessionIds().get(0));
-		if (entity.getMgiAccessionIds() != null || entity.getMgiAccessionIds().isEmpty()) {
+		if (entity.getMgiAccessionIds() != null || !entity.getMgiAccessionIds().isEmpty()) {
 			domain.setAccID(entity.getMgiAccessionIds().get(0).getAccID());
 		}
 		

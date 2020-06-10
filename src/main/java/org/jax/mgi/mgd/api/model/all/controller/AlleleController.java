a269b7c5db0775026e5e38b1d35980604c5d5c44
Lori Corbani <lori.corbani@jax.org>
2020-06-10 06:42:12 -0400

    TR13204/Infrastructure project/Alleles

--- src/main/java/org/jax/mgi/mgd/api/model/all/controller/AlleleController.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/controller/AlleleController.java
@@ -69,6 +69,7 @@ public class AlleleController extends BaseController<AlleleDomain> {
 		
 		if (domain.getMutantCellLineAssocs() != null) {
 			cellLineResults = cellLineService.createMutantCellLine(domain.getAlleleTypeKey(), domain.getMutantCellLineAssocs().get(0).getMutantCellLine(), user);
+			log.info("items: " + cellLineResults.items);
 			if (cellLineResults.items != null) {
 	    		domain.getMutantCellLineAssocs().get(0).getMutantCellLine().setCellLineKey(cellLineResults.items.get(0).getCellLineKey());        		
 			}

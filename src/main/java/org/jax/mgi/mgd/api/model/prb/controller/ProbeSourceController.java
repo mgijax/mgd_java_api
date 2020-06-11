e9b0445c73514a7f3a489897cf18511e74ce1868
Sharon Giannatto <sharon.giannatto@jax.org>
2020-06-11 16:23:21 -0400

    added search

--- src/main/java/org/jax/mgi/mgd/api/model/prb/controller/ProbeSourceController.java
+++ src/main/java/org/jax/mgi/mgd/api/model/prb/controller/ProbeSourceController.java
@@ -1,18 +1,25 @@
 package org.jax.mgi.mgd.api.model.prb.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.jax.mgi.mgd.api.model.BaseController;
 import org.jax.mgi.mgd.api.model.mgi.entities.User;
+import org.jax.mgi.mgd.api.model.mrk.domain.MarkerDomain;
+import org.jax.mgi.mgd.api.model.mrk.domain.SlimMarkerDomain;
 import org.jax.mgi.mgd.api.model.prb.domain.ProbeSourceDomain;
 import org.jax.mgi.mgd.api.model.prb.service.ProbeSourceService;
 import org.jax.mgi.mgd.api.util.SearchResults;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 
 @Path("/source")
@@ -49,6 +56,21 @@ public class ProbeSourceController extends BaseController<ProbeSourceDomain> {
 	@Override
 	public SearchResults<ProbeSourceDomain> delete(Integer key, User user) {
 		return probeSourceService.delete(key, user);
+	}
+	@POST
+	@ApiOperation(value = "Search/returns probe source domain")
+	@Path("/search")
+	public List<ProbeSourceDomain> search(ProbeSourceDomain searchDomain) {
+	
+		List<ProbeSourceDomain> results = new ArrayList<ProbeSourceDomain>();
+
+		try {
+			results = probeSourceService.search(searchDomain);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return results;
 	}
 		
 }

95cb6a41b40aba1e05db1d7f6c6875b25e227a8c
Lori Corbani <lori.corbani@jax.org>
2020-07-02 13:26:16 -0400

    TR13204/Infrastructure project/ProbeSource

--- src/main/java/org/jax/mgi/mgd/api/model/all/service/CellLineService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/service/CellLineService.java
@@ -398,7 +398,7 @@ public class CellLineService extends BaseService<CellLineDomain> {
 		
 		if (from_vector == true) {
 			from = from + ", voc_term vt";
-			where = where + "\nd._vector_key = vt._term_key";
+			where = where + "\nand d._vector_key = vt._term_key";
 		}
 		
 		if (from_parentcellline == true) {

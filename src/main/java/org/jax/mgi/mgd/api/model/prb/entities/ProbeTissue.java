3dd7d3d4d4d7c37083605af9b446bef6399fbb77
Sharon Giannatto <sharon.giannatto@jax.org>
2020-07-14 13:02:04 -0400

    added autosequence

--- src/main/java/org/jax/mgi/mgd/api/model/prb/entities/ProbeTissue.java
+++ src/main/java/org/jax/mgi/mgd/api/model/prb/entities/ProbeTissue.java
@@ -3,12 +3,16 @@ package org.jax.mgi.mgd.api.model.prb.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.jax.mgi.mgd.api.model.BaseEntity;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +23,10 @@ import lombok.Setter;
 public class ProbeTissue extends BaseEntity {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="prb_tissue_generator")
+    @SequenceGenerator(name="prb_tissue_generator", sequenceName = "prb_tissue_seq", allocationSize=1)
+    @ApiModelProperty(value="primary key")
+
 	private int _tissue_key;
 	private String tissue;
 	private int standard;

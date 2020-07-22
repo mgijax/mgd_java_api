83770a6b46cea2e6f91e5f3e2bef702d7484d734
Lori Corbani <lori.corbani@jax.org>
2020-07-22 14:33:53 -0400

    TR13204/Infrastructure project/Allele Derivation

--- src/main/java/org/jax/mgi/mgd/api/model/all/translator/AlleleCellLineDerivationTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/translator/AlleleCellLineDerivationTranslator.java
@@ -44,6 +44,7 @@ public class AlleleCellLineDerivationTranslator extends BaseEntityDomainTranslat
 		if (entity.getReference() != null) {
 			domain.setRefsKey(String.valueOf(entity.getReference().get_refs_key()));
 			domain.setJnumid(entity.getReference().getReferenceCitationCache().getJnumid());
+			domain.setJnum(entity.getReference().getReferenceCitationCache().getNumericPart());
 			domain.setShort_citation(entity.getReference().getReferenceCitationCache().getShort_citation());
 		}
 		

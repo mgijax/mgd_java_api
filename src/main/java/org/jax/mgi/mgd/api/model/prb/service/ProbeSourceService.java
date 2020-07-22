b0469873c6448f3e31d43bf246919af39204297b
Sharon Giannatto <sharon.giannatto@jax.org>
2020-07-22 16:39:06 -0400

    strain default tweaking

--- src/main/java/org/jax/mgi/mgd/api/model/prb/service/ProbeSourceService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/prb/service/ProbeSourceService.java
@@ -104,7 +104,7 @@ public class ProbeSourceService extends BaseService<ProbeSourceDomain> {
 		}
 		// Special defaults
 		// if organism not Mouse or not Not Specified, default strain is Not Applicable
-		if (!domain.getOrganismKey().equals("1") && ! domain.getOrganismKey().equals("-1") && !domain.getStrainKey().equals("-1")) {
+		if ((!domain.getOrganismKey().equals("1") || ! domain.getOrganismKey().equals("-1")) && !domain.getStrainKey().equals("-1")) {
 			domain.setStrainKey("-2");
 		}
 		//if tissue is specified (NOT Not Specified), cell line default is Not Applicable

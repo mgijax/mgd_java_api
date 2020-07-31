1e1ed234b15cff0e827dcf7e6a27395f484753d5
Sharon Giannatto <sharon.giannatto@jax.org>
2020-07-31 12:02:20 -0400

    change to checking for antigenKey

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/service/AntibodyService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/service/AntibodyService.java
@@ -348,11 +348,11 @@ public class AntibodyService extends BaseService<AntibodyDomain> {
 		if(searchDomain.getAntibodyNote() != null && ! searchDomain.getAntibodyNote().isEmpty()) {
 			where = where + "\n and a.antibodyNote ilike '" + searchDomain.getAntibodyNote() + "'";
 		}
-		if (searchDomain.getAntigen() != null && searchDomain.getAntigen().getAntigenKey() != null && ! searchDomain.getAntigen().getAntigenKey().isEmpty()) {
+		//if (searchDomain.getAntigen() != null && searchDomain.getAntigen().getAntigenKey() != null && ! searchDomain.getAntigen().getAntigenKey().isEmpty()) {
 			//if (searchDomain.getAntigen().getAntigenKey() != null && ! searchDomain.getAntigen().getAntigenKey().isEmpty()) {
-				where = where + "\n and a._antigen_key = " + searchDomain.getAntigen().getAntigenKey();
+			//	where = where + "\n and a._antigen_key = " + searchDomain.getAntigen().getAntigenKey();
 			//}
-		}
+		//}
 		else { // no antigen key check for antigen and antigen source attributes
 			if (searchDomain.getAntigen().getAntigenName() != null && ! searchDomain.getAntigen().getAntigenName().isEmpty()) {
 				where = where + "\n and av.antigenname ilike '" + searchDomain.getAntigen().getAntigenName() + "'";

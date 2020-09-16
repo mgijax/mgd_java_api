94b0a314f19cc9cd833dd50dae47031411616fc3
Lori Corbani <lori.corbani@jax.org>
2020-09-16 12:34:56 -0400

    TR13204/Infrastructure project/Alleles

--- src/main/java/org/jax/mgi/mgd/api/model/all/service/AlleleAnnotService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/service/AlleleAnnotService.java
@@ -368,10 +368,12 @@ public class AlleleAnnotService extends BaseService<DenormAlleleAnnotDomain> {
 		// use teleuse sql logic (ei/csrc/mgdsql.c/mgisql.c) 
 
 		String cmd = "";
-		String select = "select distinct v._object_key, v.description";
-		String from = "from all_summary_view v";		
-		String where = "where v._mgitype_key = " + mgiTypeKey;
-		String orderBy = "order by v._object_key, v.description";
+		String select = "select distinct v._object_key, v1.sequenceNum, a.symbol as description";
+		String from = "from all_summary_view v, , all_allele a, voc_term v1";		
+		String where = "where v._mgitype_key = " + mgiTypeKey 
+				+ "\nand v._object_key = a._allele_key"
+				+ "\nand a._allele_status_key = v1._term_key";
+		String orderBy = "order by v1.sequenceNum, description";
 		
 		String value;
 

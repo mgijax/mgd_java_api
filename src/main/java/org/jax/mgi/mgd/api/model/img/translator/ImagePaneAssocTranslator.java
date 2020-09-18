beb9e5f14e23b74a287271b5c67e7bd6947b5ef4
Lori Corbani <lori.corbani@jax.org>
2020-09-18 12:13:25 -0400

    ImagePaneAssoc/allele/genotype

--- src/main/java/org/jax/mgi/mgd/api/model/img/translator/ImagePaneAssocTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/img/translator/ImagePaneAssocTranslator.java
@@ -34,23 +34,23 @@ public class ImagePaneAssocTranslator extends BaseEntityDomainTranslator<ImagePa
 		domain.setCreation_date(dateFormatNoTime.format(entity.getCreation_date()));
 		domain.setModification_date(dateFormatNoTime.format(entity.getModification_date()));
 	
-//		// one-to-many allele associations w/ allele info
-//		if (entity.getAlleles() != null && !entity.getAlleles().isEmpty()
-//				&& entity.getMgiType().get_mgitype_key() == 11) {
-//			SlimAlleleTranslator alleleTranslator = new SlimAlleleTranslator();
-//			Iterable<SlimAlleleDomain> i = alleleTranslator.translateEntities(entity.getAlleles());
-//			domain.setAlleles(IteratorUtils.toList(i.iterator()));
-//			domain.getAlleles().sort(Comparator.comparing(SlimAlleleDomain::getSymbol, String.CASE_INSENSITIVE_ORDER));
-//		}
-//		
-//		// one-to-many genotype associations w/ genotype info
-//		if (entity.getGenotypes() != null && !entity.getGenotypes().isEmpty()
-//				&& entity.getMgiType().get_mgitype_key() == 12) {
-//			SlimGenotypeTranslator genotypeTranslator = new SlimGenotypeTranslator();
-//			Iterable<SlimGenotypeDomain> i = genotypeTranslator.translateEntities(entity.getGenotypes());
-//			domain.setGenotypes(IteratorUtils.toList(i.iterator()));
-//			//domain.getGenotypes().sort(Comparator.comparing(SlimGenotypeRefAssocDomain::getSymbol, String.CASE_INSENSITIVE_ORDER));
-//		}
+		// one-to-many allele associations w/ allele info
+		if (entity.getAlleles() != null && !entity.getAlleles().isEmpty()
+				&& entity.getMgiType().get_mgitype_key() == 11) {
+			SlimAlleleTranslator alleleTranslator = new SlimAlleleTranslator();
+			Iterable<SlimAlleleDomain> i = alleleTranslator.translateEntities(entity.getAlleles());
+			domain.setAlleles(IteratorUtils.toList(i.iterator()));
+			domain.getAlleles().sort(Comparator.comparing(SlimAlleleDomain::getSymbol, String.CASE_INSENSITIVE_ORDER));
+		}
+		
+		// one-to-many genotype associations w/ genotype info
+		if (entity.getGenotypes() != null && !entity.getGenotypes().isEmpty()
+				&& entity.getMgiType().get_mgitype_key() == 12) {
+			SlimGenotypeTranslator genotypeTranslator = new SlimGenotypeTranslator();
+			Iterable<SlimGenotypeDomain> i = genotypeTranslator.translateEntities(entity.getGenotypes());
+			domain.setGenotypes(IteratorUtils.toList(i.iterator()));
+			//domain.getGenotypes().sort(Comparator.comparing(SlimGenotypeRefAssocDomain::getSymbol, String.CASE_INSENSITIVE_ORDER));
+		}
 		
 		return domain;
 	}

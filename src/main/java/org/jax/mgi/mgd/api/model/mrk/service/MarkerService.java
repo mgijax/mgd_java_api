c7a6095a8ea0560747e7f9e720d8eab6188e659b
Lori Corbani <lori.corbani@jax.org>
2020-09-21 08:17:36 -0400

    MarkerService/mrkrefByReferenceUtilities

--- src/main/java/org/jax/mgi/mgd/api/model/mrk/service/MarkerService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mrk/service/MarkerService.java
@@ -1486,6 +1486,7 @@ public class MarkerService extends BaseService<MarkerDomain> {
 		// run the runCmd
 		log.info(Constants.LOG_INPROGRESS_EIUTILITIES + runCmd);
 		RunCommand runner = RunCommand.runCommand(runCmd);
+		log.info(Constants.LOG_INPROGRESS_EIUTILITIES + "RunCommand finished");
 		
 		// check exit code from RunCommand
 		if (runner.getExitCode() == 0) {

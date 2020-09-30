6e027248af23c9c85ab29835f42c1ddd8f587164
Lori Corbani <lori.corbani@jax.org>
2020-09-30 09:13:13 -0400

    TR13349/Genome Build 39 project

--- src/main/java/org/jax/mgi/mgd/api/model/prb/translator/ProbeTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/prb/translator/ProbeTranslator.java
@@ -46,7 +46,7 @@ public class ProbeTranslator extends BaseEntityDomainTranslator<Probe, ProbeDoma
 		if (entity.getDerivedFrom() != null && !entity.getDerivedFrom().getName().isEmpty()) {
 			log.info("translating getDerviedFrom");
 			domain.setDerivedFromKey(String.valueOf(entity.getDerivedFrom().get_probe_key()));
-//			domain.setDerivedFromName(entity.getDerivedFrom().getDerivedFrom().getName());
+			domain.setDerivedFromName(entity.getDerivedFrom().getDerivedFrom().getName());
 //			domain.setDerivedFromAccID(entity.getDerivedFrom().getDerivedFrom().getMgiAccessionIds().get(0).getAccID());
 		}
 		

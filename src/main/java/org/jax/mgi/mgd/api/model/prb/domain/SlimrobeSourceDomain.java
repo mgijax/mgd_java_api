4d7d922f45b7b1790ffe2f2ebbfca676cdb27959
Lori Corbani <lori.corbani@jax.org>
2020-10-07 14:27:28 -0400

    TR13349/Genome Build 39 project

---
+++ src/main/java/org/jax/mgi/mgd/api/model/prb/domain/SlimrobeSourceDomain.java
@@ -0,0 +1,16 @@
+package org.jax.mgi.mgd.api.model.prb.domain;
+
+import org.jax.mgi.mgd.api.model.BaseDomain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class SlimProbeStrainDomain extends BaseDomain {
+
+	private String strainKey;
+	private String strain;
+	private String isPrivate;
+	private String accID;
+	
+}

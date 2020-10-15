e708fcdc8b688a7f49ed41cadbc21a3ce2fae6fd
Lori Corbani <lori.corbani@jax.org>
2020-10-15 12:57:39 -0400

    TR13412/ltlo: lit triage reference type problem

--- src/main/java/org/jax/mgi/mgd/api/model/bib/repository/LTReferenceRepository.java
+++ src/main/java/org/jax/mgi/mgd/api/model/bib/repository/LTReferenceRepository.java
@@ -320,7 +320,14 @@ public class LTReferenceRepository extends BaseRepository<LTReferenceDomain> {
 			entity.setYear(year);
 			entity.setPgs(domain.pgs);
 			entity.setReferenceTypeTerm(termDAO.get(Integer.valueOf(domain.getReferenceTypeKey())));
-			entity.setReferenceAbstract(DecodeString.setDecodeToLatin9(domain.referenceAbstract).replace("''",  "'"));			
+			
+			if (domain.getReferenceAbstract() == null || domain.getReferenceAbstract().isEmpty()) {
+				entity.setReferenceAbstract(null);
+			}
+			else {
+				entity.setReferenceAbstract(DecodeString.setDecodeToLatin9(domain.referenceAbstract).replace("''",  "'"));			
+			}
+			
 			entity.setModificationInfo(currentUser);
 			
 			anyChanges = true;

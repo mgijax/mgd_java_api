6596b49838ab0a3517a8dbf7c1813474ae22d568
Lori Corbani <lori.corbani@jax.org>
2020-10-22 07:36:02 -0400

    TR13349/Genome Build 39 project

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/Organism.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/Organism.java
@@ -5,17 +5,22 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Where;
 import org.jax.mgi.mgd.api.model.BaseEntity;
 import org.jax.mgi.mgd.api.model.acc.entities.Accession;
+import org.jax.mgi.mgd.api.model.mrk.entities.MarkerChromosome;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,7 +31,11 @@ import lombok.Setter;
 public class Organism extends BaseEntity {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mgi_organism_generator")
+	@SequenceGenerator(name="mgi_organism_generator", sequenceName = "mgi_organism_seq", allocationSize=1)
+	@ApiModelProperty(value="primary key")
 	private int _organism_key;
+	
 	private String commonname;
 	private String latinname;
 	private Date creation_date;
@@ -46,8 +55,16 @@ public class Organism extends BaseEntity {
 	private Accession mgiAccessionId;
 
 	@OneToMany()
-	@JoinColumn(name="_object_key", referencedColumnName="_organism_key")
+	@JoinColumn(name="_object_key", referencedColumnName="_organism_key", insertable=false, updatable=false)
 	@Where(clause="`_mgitype_key` = 20 AND preferred = 1")
 	private List<Accession> allAccessionIds;
 
+	@OneToMany()
+	@JoinColumn(name="_organism_key", insertable=false, updatable=false)
+	private List<OrganismMGIType> mgiTypes;
+
+	@OneToMany()
+	@JoinColumn(name="_organism_key", insertable=false, updatable=false)
+	private List<MarkerChromosome> chromosomes;	
+	
 }
---
+++ src/main/java/org/jax/mgi/mgd/api/model/mrk/entities/MarkerChromosome.java
@@ -0,0 +1,53 @@
+package org.jax.mgi.mgd.api.model.mrk.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.jax.mgi.mgd.api.model.BaseEntity;
+import org.jax.mgi.mgd.api.model.mgi.entities.Organism;
+import org.jax.mgi.mgd.api.model.mgi.entities.User;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@ApiModel(value = "Marker Chromosome Object")
+@Table(name="mrk_chromosome")
+public class MarkerChromosome extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mrk_chromosome_generator")
+	@SequenceGenerator(name="mrk_chromosome_generator", sequenceName = "mrk_chromosome_seq", allocationSize=1)
+	@ApiModelProperty(value="primary key")
+	private int _chromosome_key;
+	
+	private String chromosome;
+	private Integer sequenceNum;
+	private Date creation_date;
+	private Date modification_date;
+
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="_organism_key")
+	private Organism organism;
+	
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="_createdby_key", referencedColumnName="_user_key")
+	private User createdBy;
+
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="_modifiedby_key", referencedColumnName="_user_key")
+	private User modifiedBy;
+
+}

b8efa3b6e93135617fd3fddac01b9d1d84b14a44
Lori Corbani <lori.corbani@jax.org>
2020-11-03 08:22:22 -0500

    TR13349/Genome Build 39 project

--- src/main/java/org/jax/mgi/mgd/api/model/mld/entities/ExptNote.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mld/entities/ExptNote.java
@@ -3,16 +3,12 @@ package org.jax.mgi.mgd.api.model.mld.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.jax.mgi.mgd.api.model.BaseEntity;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,10 +19,7 @@ import lombok.Setter;
 public class ExptNote extends BaseEntity {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mld_expt_note_generator")
-	@SequenceGenerator(name="mld_expt_note_generator", sequenceName = "mld_expt_note_seq", allocationSize=1)
-	@ApiModelProperty(value="primary key")
-	private int _expt_key;
+	private Integer _expt_key;
 	private String note;
 	private Date creation_date;
 	private Date modification_date;
--- src/main/java/org/jax/mgi/mgd/api/model/mld/entities/MappingNote.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mld/entities/MappingNote.java
@@ -3,16 +3,12 @@ package org.jax.mgi.mgd.api.model.mld.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.jax.mgi.mgd.api.model.BaseEntity;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,9 +19,6 @@ import lombok.Setter;
 public class MappingNote extends BaseEntity {
 
 	@Id
-//	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mld_note_generator")
-//	@SequenceGenerator(name="mld_note_generator", sequenceName = "mld_note_seq", allocationSize=1)
-//	@ApiModelProperty(value="primary key")
 	private Integer _refs_key;
 	private String note;
 	private Date creation_date;

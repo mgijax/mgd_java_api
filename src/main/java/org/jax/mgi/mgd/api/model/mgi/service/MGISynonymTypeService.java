409f5a094df7766436881d6c4f49d5c60a62e5c5
Lori Corbani <lori.corbani@jax.org>
2020-12-08 14:05:56 -0500

    TR13349/Genome Build 39 project

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/service/MGISynonymTypeService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/service/MGISynonymTypeService.java
@@ -76,8 +76,7 @@ public class MGISynonymTypeService extends BaseService<MGISynonymTypeDomain> {
 		List<MGISynonymTypeDomain> results = new ArrayList<MGISynonymTypeDomain>();
 		
 		String cmd = "select * from mgi_synonymtype"
-			+ "\nwhere _organism_key = 1"
-			+ "\nand _mgitype_key = " + searchDomain.getMgiTypeKey()
+			+ "\nwhere _mgitype_key = " + searchDomain.getMgiTypeKey()
 			+ "\norder by _mgitype_key, synonymtype";
 		log.info(cmd);
 

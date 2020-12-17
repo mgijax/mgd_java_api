7406dee21ccf07d69474685388d03ce3326e57bf
Lori Corbani <lori.corbani@jax.org>
2020-12-17 10:32:30 -0500

    TR13349/Genome Build 39 project

--- src/main/java/org/jax/mgi/mgd/api/model/prb/controller/ProbeStrainController.java
+++ src/main/java/org/jax/mgi/mgd/api/model/prb/controller/ProbeStrainController.java
@@ -7,7 +7,6 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -155,7 +154,7 @@ public class ProbeStrainController extends BaseController<ProbeStrainDomain> {
 		return probeStrainService.getByRef(key);
 	}
 
-	@PUT
+	@POST
 	@ApiOperation(value = "Process Strain Merge/returns slim probe strain domain")
 	@Path("/processMerge")
 	public SearchResults<SlimProbeStrainDomain> processMerge(ProbeStrainMergeDomain mergeDomain) {

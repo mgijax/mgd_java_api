bb6b921371551d7070c9413ec8dff9fae45f1a50
Lori Corbani <lori.corbani@jax.org>
2021-09-09 12:53:10 -0400

    yaks/assay

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/service/GelRowService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/service/GelRowService.java
@@ -170,7 +170,7 @@ public class GelRowService extends BaseService<GelRowDomain> {
 				// process gxd_gellane/gxd_gelband
 				if (laneDomain != null && !laneDomain.isEmpty()) {
 					for (int j = 0; j < laneDomain.size(); j++) {
-						if (gelBandService.process(entity.get_gelrow_key(), laneDomain.get(j).getGelBands(), user)) {
+						if (gelBandService.process(Integer.valueOf(rowDomain.get(i).getGelRowKey()), laneDomain.get(j).getGelBands(), user)) {
 							modified = true;
 						}
 					}

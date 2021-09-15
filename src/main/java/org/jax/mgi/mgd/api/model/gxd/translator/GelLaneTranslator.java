c8206ee5dbd24fbe78297cf04ecdf44c84abc515
Lori Corbani <lori.corbani@jax.org>
2021-09-15 07:05:40 -0400

    yaks/assay

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/translator/GelLaneTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/translator/GelLaneTranslator.java
@@ -91,15 +91,15 @@ public class GelLaneTranslator extends BaseEntityDomainTranslator<GelLane, GelLa
 			domain.setGelBands(IteratorUtils.toList(i.iterator()));
 			domain.getGelBands().sort(Comparator.comparingInt(GelBandDomain::getSequenceNum));			
 		}
-//		else {
-//			List<GelBandDomain> gelBands = new ArrayList<GelBandDomain>();
-//			GelBandDomain gelBand = new GelBandDomain();
-//			gelBand.setProcessStatus(Constants.PROCESS_CREATE);
-//			gelBand.setGelLaneKey(String.valueOf(entity.get_gellane_key()));
-//			gelBand.setAssayKey(String.valueOf(entity.get_assay_key()));							
-//			gelBands.add(gelBand);
-//			domain.setGelBands(gelBands);
-//		}
+		else {
+			List<GelBandDomain> gelBands = new ArrayList<GelBandDomain>();
+			GelBandDomain gelBand = new GelBandDomain();
+			gelBand.setProcessStatus(Constants.PROCESS_CREATE);
+			gelBand.setGelLaneKey(String.valueOf(entity.get_gellane_key()));
+			gelBand.setAssayKey(String.valueOf(entity.get_assay_key()));							
+			gelBands.add(gelBand);
+			domain.setGelBands(gelBands);
+		}
 		
 		return domain;
 	}

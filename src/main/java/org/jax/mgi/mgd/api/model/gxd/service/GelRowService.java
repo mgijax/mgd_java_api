819f70eec2d26aabaa6b003423b59430f07cf0ce
Lori Corbani <lori.corbani@jax.org>
2021-09-29 14:02:49 -0400

    yaks/assay

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/service/GelRowService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/service/GelRowService.java
@@ -129,7 +129,7 @@ public class GelRowService extends BaseService<GelRowDomain> {
 				// process gxd_gellane/gxd_gelband
 				if (laneDomain != null && !laneDomain.isEmpty()) {
 					for (int j = 0; j < laneDomain.size(); j++) {
-						if (laneDomain.get(j).getLaneLabel() != null && !laneDomain.get(j).getLaneLabel().isEmpty()) {
+						if (laneDomain.get(j).getGelControlKey() != null && !laneDomain.get(j).getGelControlKey().isEmpty()) {
 							modified = gelBandService.process(entity.get_gelrow_key(), Integer.valueOf(laneDomain.get(j).getGelLaneKey()), rowDomain.get(i).getSequenceNum(), laneDomain.get(j).getGelBands(), user);
 						}
 					}
@@ -173,7 +173,7 @@ public class GelRowService extends BaseService<GelRowDomain> {
 				// process gxd_gellane/gxd_gelband
 				if (laneDomain != null && !laneDomain.isEmpty()) {
 					for (int j = 0; j < laneDomain.size(); j++) {
-						if (laneDomain.get(j).getLaneLabel() != null && !laneDomain.get(j).getLaneLabel().isEmpty()) {
+						if (laneDomain.get(j).getGelControlKey() != null && !laneDomain.get(j).getGelControlKey().isEmpty()) {
 							modified = gelBandService.process(Integer.valueOf(rowDomain.get(i).getGelRowKey()), Integer.valueOf(laneDomain.get(j).getGelLaneKey()), rowDomain.get(i).getSequenceNum(), laneDomain.get(j).getGelBands(), user);
 						}
 					}

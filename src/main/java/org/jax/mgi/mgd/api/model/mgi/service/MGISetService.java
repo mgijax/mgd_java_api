da1bc4d5b50febb49397fa9ddc13941b3d490582
Sharon Giannatto <sharon.giannatto@jax.org>
2021-10-20 15:54:08 -0400

    added endpoint for returning set by set/user/sequenceNum order

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/service/MGISetService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/service/MGISetService.java
@@ -119,23 +119,17 @@ public class MGISetService extends BaseService<MGISetDomain> {
 
 		List<MGISetDomain> results = new ArrayList<MGISetDomain>();
 		List<MGISetMemberDomain> listOfMembers = new ArrayList<MGISetMemberDomain>();
-		
-		String orderBy = "s.label";
 				
 		MGISetDomain domain = new MGISetDomain();
 		domain.setSetKey(searchDomain.getSetKey());
 		domain.setCreatedBy(searchDomain.getCreatedBy());
-		
-		if (domain.getSetKey().equals("1059")) {
-			orderBy = "s.sequenceNum";
-		}
 		
 		String cmd = "\nselect s.* from mgi_setmember s, mgi_user u"
 				+ "\nwhere s._createdby_key = u._user_key" 
 				+ "\nand s._set_key = " + searchDomain.getSetKey()
 				+ "\nand s._createdby_key = u._user_key"
 				+ "\nand u.login = '" + searchDomain.getCreatedBy() + "'"
-				+ "\norder by " + orderBy;
+				+ "\norder by s.label ";
 		log.info(cmd);
 
 		try {
@@ -161,5 +155,50 @@ public class MGISetService extends BaseService<MGISetDomain> {
 		results.add(domain);
 		return results;
 	}
+	
+	@Transactional	
+	public List<MGISetDomain> getBySetUserBySeqNum(MGISetDomain searchDomain) {
+		// return all set members by _set_key, _createdby_key
+
+		List<MGISetDomain> results = new ArrayList<MGISetDomain>();
+		List<MGISetMemberDomain> listOfMembers = new ArrayList<MGISetMemberDomain>();
+		
+				
+		MGISetDomain domain = new MGISetDomain();
+		domain.setSetKey(searchDomain.getSetKey());
+		domain.setCreatedBy(searchDomain.getCreatedBy());
+		
+		String cmd = "\nselect s.* from mgi_setmember s, mgi_user u"
+				+ "\nwhere s._createdby_key = u._user_key" 
+				+ "\nand s._set_key = " + searchDomain.getSetKey()
+				+ "\nand s._createdby_key = u._user_key"
+				+ "\nand u.login = '" + searchDomain.getCreatedBy() + "'"
+				+ "\norder by s.sequenceNum";
+		log.info(cmd);
+
+		try {
+			ResultSet rs = sqlExecutor.executeProto(cmd);
+			while (rs.next()) {
+				MGISetMemberDomain memberDomain = new MGISetMemberDomain();
+				memberDomain = memberTranslator.translate(setMemberDAO.get(rs.getInt("_setmember_key")));
+				setMemberDAO.clear();
+				listOfMembers.add(memberDomain);
+			}
+			sqlExecutor.cleanup();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if (domain.getSetKey().equals("1055")) {
+			domain.setGenotypeClipboardMembers(listOfMembers);
+		}
+		else if (domain.getSetKey().equals("1059")) {
+			domain.setCelltypeClipboardMembers(listOfMembers);
+		}
+		results.add(domain);
+		return results;
+	}
+
 
 }

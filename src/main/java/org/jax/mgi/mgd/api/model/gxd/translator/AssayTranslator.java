1f0afde82f2439f5adb2757ee386d3ff7187d4ab
Lori Corbani <lori.corbani@jax.org>
2021-10-21 11:36:27 -0400

    yaks/assay

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/translator/AssayTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/translator/AssayTranslator.java
@@ -112,7 +112,7 @@ public class AssayTranslator extends BaseEntityDomainTranslator<Assay, AssayDoma
 		if (entity.getImagePane() != null) {
 			ImagePaneTranslator i = new ImagePaneTranslator();
 			domain.setImagePane(i.translate(entity.getImagePane()));
-			domain.setImagePaneKey(domain.getImagePane().getImageKey());
+			domain.setImagePaneKey(domain.getImagePane().getImagePaneKey());
 		}
 		
 		// reporter gene

c11451913fccc69729c2db2c825ebac7932f9937
Sharon Giannatto <sharon.giannatto@jax.org>
2021-11-09 11:51:22 -0500

    updated entity for the dag stack

--- src/main/java/org/jax/mgi/mgd/api/model/dag/entities/DagEdge.java
+++ src/main/java/org/jax/mgi/mgd/api/model/dag/entities/DagEdge.java
@@ -24,8 +24,8 @@ public class DagEdge extends BaseEntity {
 	@Id
 	private int _edge_key;
 	private Integer sequenceNum;
-	private Date creation_date;
-	private Date modification_date;
+	private String creation_date;
+	private String modification_date;
 
 	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_dag_key")

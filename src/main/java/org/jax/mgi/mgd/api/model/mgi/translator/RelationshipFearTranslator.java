039afd78e6ea1feba2e559756da21b49568fed21
Lori Corbani <lori.corbani@jax.org>
2022-03-14 08:36:19 -0400

    cre2022/allele relationship (allelefear)

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/translator/RelationshipFearTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/translator/RelationshipFearTranslator.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 
 import org.apache.commons.collections4.IteratorUtils;
 import org.jax.mgi.mgd.api.model.BaseEntityDomainTranslator;
-import org.jax.mgi.mgd.api.model.mgi.domain.MGISynonymDomain;
 import org.jax.mgi.mgd.api.model.mgi.domain.NoteDomain;
 import org.jax.mgi.mgd.api.model.mgi.domain.RelationshipFearDomain;
 import org.jax.mgi.mgd.api.model.mgi.domain.RelationshipPropertyDomain;

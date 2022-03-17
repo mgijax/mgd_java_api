b49a559997c11cb0fb5e6bd19c0167725fcc9372
Lori Corbani <lori.corbani@jax.org>
2022-03-17 14:29:19 -0400

    cre2022/allele relationship (allelefear)

--- src/main/java/org/jax/mgi/mgd/api/model/all/domain/SlimAlleleFearRegionDomain.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/domain/SlimAlleleFearRegionDomain.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Getter @Setter
 public class SlimAlleleFearRegionDomain extends BaseDomain {
 	
-	// used by service/getMarkerByRegion()
+	// used by service/allelefear/getMarkerByRegion()
 	
 	private String markerKey;
 	private String alleleKey;

21b32e9e83dd847dacff1655c7aa80500e1a9c6c
Lori Corbani <lori.corbani@jax.org>
2022-04-15 13:07:07 -0400

    wts2-574/TR13474 - Lit Triage: Search by associated gene, allele, strain

--- src/main/java/org/jax/mgi/mgd/api/model/bib/dao/LTReferenceDAO.java
+++ src/main/java/org/jax/mgi/mgd/api/model/bib/dao/LTReferenceDAO.java
@@ -56,28 +56,20 @@ public class LTReferenceDAO extends PostgresSQLDAO<LTReference> {
 	 */
 	@Transactional
 	public LTReference getReference(String refsKey) throws APIException {
+		
+		if (refsKey == null || refsKey.isEmpty()) {return null;}
 		LTReference ref =  entityManager.find(LTReference.class, Integer.valueOf(refsKey));
 		if (ref == null) { return null; }
-		log.info("LTReferenceDAO:getReference(String refsKey):" + refsKey);
-		log.info("getWorkflowTags()");
+		
 		Hibernate.initialize(ref.getWorkflowTags().size());
-		log.info("getReferenceTypeTerm()");
 		Hibernate.initialize(ref.getReferenceTypeTerm());
-		log.info("getNotes()");
 		Hibernate.initialize(ref.getNotes());
-		log.info("getReferenceBook()");
 		Hibernate.initialize(ref.getReferenceBook());
-		log.info("getCreatedByUser()");
 		Hibernate.initialize(ref.getCreatedByUser());
-		log.info("getModifiedByUser()");
 		Hibernate.initialize(ref.getModifiedByUser());
-		log.info("getAssociatedData()");
 		Hibernate.initialize(ref.getAssociatedData());
-		log.info("getAccessionIDs()");
 		Hibernate.initialize(ref.getAccessionIDs());
-		log.info("getWorkflowData()");
 		Hibernate.initialize(ref.getWorkflowData());
-		log.info("getWorkflowStatuses()");
 		Hibernate.initialize(ref.getWorkflowStatuses());
 		return ref;
 	}

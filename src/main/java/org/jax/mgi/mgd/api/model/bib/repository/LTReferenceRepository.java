70bed63d211d7a926305102e85b6dc486af33adf
Lori Corbani <lori.corbani@jax.org>
2022-05-09 06:58:19 -0400

    lit triage/merge LT to Reference

--- src/main/java/org/jax/mgi/mgd/api/model/bib/repository/LTReferenceRepository.java
+++ src/main/java/org/jax/mgi/mgd/api/model/bib/repository/LTReferenceRepository.java
@@ -612,12 +612,12 @@ public class LTReferenceRepository extends BaseRepository<LTReferenceDomain> {
 
 		log.info("applyWorkflowRelevanceChanges()");
 		
-		log.info("domain.getEditRelevanceKey();" + domain.getEditRelevanceKey());
-		log.info("user.getLogin();:" + entity.getWorkflowRelevance().get(0).getModifiedBy() + "," + user.getLogin());
+		log.info("domain.getEditRelevanceKey():" + domain.getEditRelevanceKey());
+		log.info("user.getLogin();:" + entity.getWorkflowRelevance().get(0).getModifiedBy().getLogin() + "," + user.getLogin());
 		
 		// if relevance term has changed or user has changed
 		if (!smartEqual(String.valueOf(entity.getWorkflowRelevance().get(0).getRelevanceTerm().get_term_key()), domain.getEditRelevanceKey())
-			|| !smartEqual(entity.getWorkflowRelevance().get(0).getModifiedBy(), user.getLogin())
+			|| !smartEqual(entity.getWorkflowRelevance().get(0).getModifiedBy().getLogin(), user.getLogin())
 			) {
 
 			// for each relevanceHistory, set processStatus = PROCESS_UPDATE, isCurrent = 0

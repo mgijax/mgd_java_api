8088617932e32d8b5bdd9ce1b1ed5551bb647488
Lori Corbani <lori.corbani@jax.org>
2022-05-13 08:32:00 -0400

    lit triage/merge LT to Reference

--- src/main/java/org/jax/mgi/mgd/api/model/bib/service/ReferenceService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/bib/service/ReferenceService.java
@@ -1629,6 +1629,7 @@ public class ReferenceService extends BaseService<ReferenceDomain> {
 			rwTag.setModification_date(new Date());
 			referenceDAO.persist(rwTag);
 			entity.getWorkflowTags().add(rwTag);
+			referenceDAO.persist(entity);
 		}
 	}
 

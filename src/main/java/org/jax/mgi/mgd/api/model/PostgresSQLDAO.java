642456a2f9df9959beb03d6c4398c75563c81f06
Lori Corbani <lori.corbani@jax.org>
2022-05-16 11:13:35 -0400

    lit triage/merge LT to Reference

--- src/main/java/org/jax/mgi/mgd/api/model/PostgresSQLDAO.java
+++ src/main/java/org/jax/mgi/mgd/api/model/PostgresSQLDAO.java
@@ -12,7 +12,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.Id;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Path;

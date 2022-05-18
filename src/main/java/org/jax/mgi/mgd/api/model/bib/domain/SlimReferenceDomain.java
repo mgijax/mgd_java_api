c39ee61fde7bdc93f7960fa01513dc9e891d4c2a
Lori Corbani <lori.corbani@jax.org>
2022-05-18 06:38:13 -0400

    lit triage/merge LT to Reference

--- src/main/java/org/jax/mgi/mgd/api/model/bib/domain/SlimReferenceDomain.java
+++ src/main/java/org/jax/mgi/mgd/api/model/bib/domain/SlimReferenceDomain.java
@@ -34,6 +34,7 @@ public class SlimReferenceDomain extends BaseDomain {
 	public String qtl_status;
 	public String tumor_status;
 	public String haspdf;
+	public String has_new_tag;
 	
 	// used by validateJnumImage
 	private List<TermDomain> journalLicenses;

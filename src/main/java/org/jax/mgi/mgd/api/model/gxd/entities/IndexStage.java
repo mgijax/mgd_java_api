7e962c9799db8e1d133c3bff5f177e823ab2bddc
Lori Corbani <lori.corbani@jax.org>
2022-05-27 11:12:33 -0400

    add LAZY to all OneToOne in entity

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/entities/IndexStage.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/entities/IndexStage.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -26,11 +27,11 @@ public class IndexStage extends BaseEntity {
 	private Date creation_date;
 	private Date modification_date;
 	
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_createdby_key", referencedColumnName="_user_key")
 	private User createdBy;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_modifiedby_key", referencedColumnName="_user_key")
 	private User modifiedBy;
 	

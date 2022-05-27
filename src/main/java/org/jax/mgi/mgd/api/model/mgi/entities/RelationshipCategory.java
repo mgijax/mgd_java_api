8f4de9caae135a31a482ecf72aef243948928eb3
Lori Corbani <lori.corbani@jax.org>
2022-05-27 10:55:43 -0400

    add LAZY to all OneToOne in entity

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/RelationshipCategory.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/RelationshipCategory.java
@@ -3,6 +3,7 @@ package org.jax.mgi.mgd.api.model.mgi.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -28,35 +29,35 @@ public class RelationshipCategory extends BaseEntity {
 	private Date creation_date;
 	private Date modification_date;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_relationshipvocab_key", referencedColumnName="_vocab_key")
 	private Vocabulary relationshipVocab;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_relationshipdag_key", referencedColumnName="_dag_key")
 	private Dag relationshipDag ;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_mgitype_key_1", referencedColumnName="_mgitype_key")
 	private MGIType mgitype1;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_mgitype_key_2", referencedColumnName="_mgitype_key")
 	private MGIType mgitype2;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_qualifiervocab_key", referencedColumnName="_vocab_key")
 	private Vocabulary qualifierVocab;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_evidencevocab_key", referencedColumnName="_vocab_key")
 	private Vocabulary evidenceVocab;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_createdby_key", referencedColumnName="_user_key")
 	private User createdBy;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_modifiedby_key", referencedColumnName="_user_key")
 	private User modifiedBy;
 
--- src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/RelationshipProperty.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/entities/RelationshipProperty.java
@@ -3,6 +3,7 @@ package org.jax.mgi.mgd.api.model.mgi.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -34,15 +35,15 @@ public class RelationshipProperty extends BaseEntity {
 	private Date creation_date;
 	private Date modification_date;
 	
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_propertyname_key", referencedColumnName="_term_key")
 	private Term propertyName;
 	
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_createdby_key", referencedColumnName="_user_key")
 	private User createdBy;
 
-	@OneToOne
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="_modifiedby_key", referencedColumnName="_user_key")
 	private User modifiedBy;
 	

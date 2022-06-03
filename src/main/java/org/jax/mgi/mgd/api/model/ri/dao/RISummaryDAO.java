9bc195d712f135dc1aa2961aab0d4eb6a75d605e
Lori Corbani <lori.corbani@jax.org>
2022-06-03 07:57:20 -0400

    wts883/gxd_index & other ReferenceCitationCache updates

--- src/main/java/org/jax/mgi/mgd/api/model/ri/dao/RISummaryDAO.java
+++
@@ -1,9 +0,0 @@
-package org.jax.mgi.mgd.api.model.ri.dao;
-
-import org.jax.mgi.mgd.api.model.PostgresSQLDAO;
-
-public class RISummaryDAO extends PostgresSQLDAO<RISummaryDAO> {
-	protected RISummaryDAO() {
-		super(RISummaryDAO.class);
-	}
-}
--- src/main/java/org/jax/mgi/mgd/api/model/ri/dao/RISummaryExptRefDAO.java
+++
@@ -1,9 +0,0 @@
-package org.jax.mgi.mgd.api.model.ri.dao;
-
-import org.jax.mgi.mgd.api.model.PostgresSQLDAO;
-
-public class RISummaryExptRefDAO extends PostgresSQLDAO<RISummaryExptRefDAO> {
-	protected RISummaryExptRefDAO() {
-		super(RISummaryExptRefDAO.class);
-	}
-}

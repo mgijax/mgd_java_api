2df0bfdf1c6ce2caaa3326cb83b9d7e9eec425b6
Lori Corbani <lori.corbani@jax.org>
2022-06-03 07:58:26 -0400

    wts883/gxd_index & other ReferenceCitationCache updates

--- src/main/java/org/jax/mgi/mgd/api/model/map/dao/CoordinateDAO.java
+++
@@ -1,9 +0,0 @@
-package org.jax.mgi.mgd.api.model.map.dao;
-
-import org.jax.mgi.mgd.api.model.PostgresSQLDAO;
-
-public class CoordinateDAO extends PostgresSQLDAO<CoordinateDAO> {
-	public CoordinateDAO() {
-		super(CoordinateDAO.class);
-	}
-}

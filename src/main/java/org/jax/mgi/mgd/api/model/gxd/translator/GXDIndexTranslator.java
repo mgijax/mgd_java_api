69cbb4778366b4173728dbad5f327c83234cf29b
Lori Corbani <lori.corbani@jax.org>
2022-06-08 10:40:59 -0400

    wts883/gxd_index & other ReferenceCitationCache updates

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/translator/GXDIndexTranslator.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/translator/GXDIndexTranslator.java
@@ -16,41 +16,36 @@ public class GXDIndexTranslator extends BaseEntityDomainTranslator<GXDIndex, GXD
 
 		GXDIndexDomain domain = new GXDIndexDomain();
 
-		log.info("inside translator");
 		domain.setIndexKey(String.valueOf(entity.get_index_key()));
 		domain.setComments(entity.getComments());
-		log.info("priority");
 		domain.setPriorityKey(String.valueOf(entity.getPriority().get_term_key()));
 		domain.setPriority(entity.getPriority().getTerm());
-		log.info("conditional");
 		domain.setConditionalMutantsKey(String.valueOf(entity.getConditionalMutants().get_term_key()));
 		domain.setConditionalMutants(entity.getConditionalMutants().getTerm());
-		log.info("reference");
 		domain.setRefsKey(String.valueOf(entity.getReference().get_refs_key()));
 		domain.setJnumid(entity.getReference().getJnumid());
 		domain.setJnum(String.valueOf(entity.getReference().getNumericPart()));
 		domain.setShort_citation(entity.getReference().getShort_citation());
-		log.info("marker");
 		domain.setMarkerKey(String.valueOf(entity.getMarker().get_marker_key()));
 		domain.setMarkerSymbol(entity.getMarker().getSymbol());		
 		domain.setMarkerName(entity.getMarker().getName());
 		domain.setMarkerChromosome(entity.getMarker().getChromosome());
 		domain.setMarkerAccID(entity.getMarker().getMgiAccessionIds().get(0).getAccID());
-		log.info("creation");
 		domain.setCreatedByKey(entity.getCreatedBy().get_user_key().toString());
 		domain.setCreatedBy(entity.getCreatedBy().getLogin());
-		log.info("modifiation");
 		domain.setModifiedByKey(entity.getModifiedBy().get_user_key().toString());
 		domain.setModifiedBy(entity.getModifiedBy().getLogin());
 		domain.setCreation_date(dateFormatNoTime.format(entity.getCreation_date()));
 		domain.setModification_date(dateFormatNoTime.format(entity.getModification_date()));
 
 		// stages
-//		if (entity.getImageStages() != null || !entity.getImageStages().isEmpty()) {
-//			GXDIndexStageTranslator stageTranslator = new GXDIndexStageTranslator();
-//			Iterable<GXDIndexStageDomain> i = stageTranslator.translateEntities(entity.getImageStages());
-//			domain.setIndexStages(IteratorUtils.toList(i.iterator()));
-//		}
+		log.info("start stages");
+		if (entity.getImageStages() != null || !entity.getImageStages().isEmpty()) {
+			GXDIndexStageTranslator stageTranslator = new GXDIndexStageTranslator();
+			Iterable<GXDIndexStageDomain> i = stageTranslator.translateEntities(entity.getImageStages());
+			domain.setIndexStages(IteratorUtils.toList(i.iterator()));
+		}
+		log.info("end stages");
 
 		// is the reference full-code/exists in expression cache
 //		if (entity.getExpressionCache() != null || !entity.getExpressionCache().isEmpty()) {
@@ -60,7 +55,6 @@ public class GXDIndexTranslator extends BaseEntityDomainTranslator<GXDIndex, GXD
 //			domain.setIsFullCoded("0");
 //		}
 		
-		log.info("returning from translator");
 		return domain;
 	}
 

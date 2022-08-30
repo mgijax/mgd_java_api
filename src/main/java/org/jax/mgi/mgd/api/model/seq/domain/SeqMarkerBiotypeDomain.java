61dd485fec48ba5f0caa8673f42a573edee2e5b4
Lori Corbani <lori.corbani@jax.org>
2022-08-30 13:37:13 -0400

    crm-395/wts2-888/PWI: add terms to API for PWI/Detail

--- src/main/java/org/jax/mgi/mgd/api/model/seq/domain/SeqMarkerBiotypeDomain.java
+++ src/main/java/org/jax/mgi/mgd/api/model/seq/domain/SeqMarkerBiotypeDomain.java
@@ -7,27 +7,13 @@ import lombok.Setter;
 
 @Getter @Setter
 public class SeqMarkerBiotypeDomain extends BaseDomain {
-	
+		
 	private String cacheKey;
-    //private String markerKey;
-    //private String sequenceKey;
-    //private String organismKey;
-    //private String refsKey; 
-    //private String qualifierKkey;
     private String sequenceProviderKey;
     private String sequenceProvider;
-    //private String sequenceTypeKey;
-    //private String logicalDBKey;
-    //private String markerTypeKey;
     private String biotypeConflictKey;
     private Boolean isBiotypeConflict;
     private String accID;
     private String rawbiotype;   
-	//private String createdByKey;
-	//private String createdBy;
-	//private String modifiedByKey;
-	//private String modifiedBy;
-	//private String creation_date;
-	//private String modification_date;
 
 }

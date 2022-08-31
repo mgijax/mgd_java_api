b195d624f7b50e6d52444682903eaac48c58fefa
Lori Corbani <lori.corbani@jax.org>
2022-08-31 07:34:10 -0400

    crm-395/wts2-888/PWI: add terms to API for PWI/Detail

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/service/AntibodyService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/service/AntibodyService.java
@@ -293,9 +293,9 @@ public class AntibodyService extends BaseService<AntibodyDomain> {
 	        
 	        // determine hasExpression
 	    	String cmd = "select case when exists (select 1 from gxd_antibodyprep p, gxd_assay e" +
-	    				"\nwhere a._antibody_key = p._antibody_key and p._antibodyprep_key = e._antibodyprep_key)" +  				
-	    				"\nthen 1 else 0 end as hasExpression" + 
-	    				"\nfrom gxd_antibody a where a._antibody_key = " + key;	
+	    				"\nwhere where p._antibody_key = " + key +
+	    				"\nand p._antibodyprep_key = e._antibodyprep_key)" +
+	    				"\nthen 1 else 0 end as hasExpression";
 	    	log.info(cmd);	
 	    	try {
 	    		ResultSet rs = sqlExecutor.executeProto(cmd);

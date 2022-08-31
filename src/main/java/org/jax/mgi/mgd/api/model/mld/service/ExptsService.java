b5b5ab0ae2e7751b17dc34aca610d8d391041e0f
Lori Corbani <lori.corbani@jax.org>
2022-08-31 13:16:37 -0400

    crm-396/wts2-888/PWI: add terms to API for PWI/Summary

--- src/main/java/org/jax/mgi/mgd/api/model/mld/service/ExptsService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mld/service/ExptsService.java
@@ -324,6 +324,7 @@ public class ExptsService extends BaseService<ExptsDomain> {
 				"\nand m._marker_key = em._marker_key" +
 				"\nand em._expt_key = e._expt_key" +
 				"\nand e._refs_key = c._refs_key" +
+				"\nand e.exptType in ('TEXT-QTL', 'TEXT-Physical Mapping', 'TEXT-Congenic', 'TEXT-QTL-Candidate Genes', 'TEXT-Meta Analysis')" +
 				"\norder by expttype, numericpart";
 		
 		log.info(cmd);	
@@ -356,6 +357,7 @@ public class ExptsService extends BaseService<ExptsDomain> {
 				"\nfrom bib_citation_cache aa, mld_expts e" + 
 				"\nwhere aa.jnumid = '" + jnumid + "'" +
 				"\nand aa._refs_key = e._refs_key" +
+				"\nand e.exptType in ('TEXT-QTL', 'TEXT-Physical Mapping', 'TEXT-Congenic', 'TEXT-QTL-Candidate Genes', 'TEXT-Meta Analysis')" +
 				"\norder by expttype, numericpart";
 		
 		log.info(cmd);	

dba24501786e4f34e5c357cf7bdf953f7af2d85c
Lori Corbani <lori.corbani@jax.org>
2022-09-15 06:42:03 -0400

    wts2-888/crm-410/API: use VOC_TermFamily views/add to endpoints to TermService

--- src/main/java/org/jax/mgi/mgd/api/model/voc/domain/TermDomain.java
+++ src/main/java/org/jax/mgi/mgd/api/model/voc/domain/TermDomain.java
@@ -18,6 +18,7 @@ public class TermDomain extends BaseDomain {
 	private String termKey;
 	private String term;
 	private String vocabKey;
+	private String vocabName;
 	private String abbreviation;
 	private String note;
 	private String sequenceNum;

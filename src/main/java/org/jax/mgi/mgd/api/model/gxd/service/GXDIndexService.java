3069e7afeefd6845acb599f54eebd45555bd66b0
Lori Corbani <lori.corbani@jax.org>
2022-10-20 12:43:11 -0400

    wts2-1023/use DecodeString for comments

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/service/GXDIndexService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/service/GXDIndexService.java
@@ -111,7 +111,7 @@ public class GXDIndexService extends BaseService<GXDIndexDomain> {
 		}
 		else {
 			String note = DecodeString.setDecodeToLatin9(domain.getComments());
-			note.replaceAll("\\u0001", "");
+			note.replaceAll("\u0001", "");
 			domain.setComments(note);
 			entity.setComments(domain.getComments());
 		}

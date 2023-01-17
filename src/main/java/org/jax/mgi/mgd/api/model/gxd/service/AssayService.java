484eaaa4200a8719c33e0520e096812deea589c7
Lori Corbani <lori.corbani@jax.org>
2023-01-17 09:59:49 -0500

    wts2-888/fl2-129/PWI page revamp: Assay and Assay/Result Summary - API component

--- src/main/java/org/jax/mgi/mgd/api/model/gxd/service/AssayService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/gxd/service/AssayService.java
@@ -1281,7 +1281,7 @@ public class AssayService extends BaseService<AssayDomain> {
 
 		List<SlimAssayDomain> results = new ArrayList<SlimAssayDomain>();
 		
-		String cmd = "\n(select distinct s._assay_key, m._marker_key, m.symbol, t1.assaytype, b.short_citation, ag.accid" + 
+		String cmd = "\n(select distinct s._assay_key, m._marker_key, m.symbol, ga._assaytype_key, t1.assaytype, b.short_citation, ag.accid" + 
 				"\nfrom all_allele a, acc_accession aa, gxd_allelegenotype g, gxd_gellane s, gxd_assay ga, mrk_marker m, gxd_assaytype t1, bib_citation_cache b, acc_accession ag" + 
 				"\nwhere a._allele_key = aa._object_key" + 
 				"\nand aa._mgitype_key = 11" +
@@ -1295,7 +1295,7 @@ public class AssayService extends BaseService<AssayDomain> {
 				"\nand ga._assay_key = ag._object_key" +
 				"\nand ag._mgitype_key = 8" +
 				"\nunion" +
-				"\nselect distinct s._assay_key, m._marker_key, m.symbol, t1.assaytype, b.short_citation, ag.accid" +
+				"\nselect distinct s._assay_key, m._marker_key, m.symbol, ga._assaytype_key, t1.assaytype, b.short_citation, ag.accid" +
 				"\nfrom all_allele a, acc_accession aa, gxd_allelegenotype g, gxd_specimen s, gxd_assay ga, mrk_marker m, gxd_assaytype t1, bib_citation_cache b, acc_accession ag" + 
 				"\nwhere a._allele_key = aa._object_key" + 
 				"\nand aa._mgitype_key = 11" +
@@ -1309,7 +1309,7 @@ public class AssayService extends BaseService<AssayDomain> {
 				"\nand ga._assay_key = ag._object_key" +
 				"\nand ag._mgitype_key = 8" +				
 				"\n)" +
-				"\norder by symbol, assaytype, short_citation, accid";
+				"\norder by symbol, _assaytype_key, short_citation, accid";
 		
 		log.info(cmd);	
 		
@@ -1336,15 +1336,17 @@ public class AssayService extends BaseService<AssayDomain> {
 
 		List<SlimAssayDomain> results = new ArrayList<SlimAssayDomain>();
 		
-		String cmd = "\nselect distinct g._assay_key, m._marker_key, m.symbol, m.symbol, g._assaytype_key, t1.assaytype, b.short_citation" + 
-				"\nfrom mrk_marker m, acc_accession aa, gxd_assay g, gxd_assaytype t1, bib_citation_cache b" + 
+		String cmd = "\nselect distinct g._assay_key, m._marker_key, m.symbol, m.symbol, g._assaytype_key, t1.assaytype, b.short_citation, ag.accid" + 
+				"\nfrom mrk_marker m, acc_accession aa, gxd_assay g, gxd_assaytype t1, bib_citation_cache b, acc_accession ag" + 
 				"\nwhere m._marker_key = aa._object_key" + 
 				"\nand aa._mgitype_key = 2" +
 				"\nand aa.accid = '" + accid + "'" +
 				"\nand m._marker_key = g._marker_key" +
 				"\nand g._assaytype_key = t1._assaytype_key" +
 				"\nand g._refs_key = b._refs_key" +
-				"\norder by m.symbol, g._assaytype_key, b.short_citation";
+				"\nand g._assay_key = ag._object_key" +
+				"\nand ag._mgitype_key = 8" +				
+				"\norder by m.symbol, g._assaytype_key, b.short_citation, aa.accid";
 		
 		log.info(cmd);	
 		
@@ -1371,12 +1373,12 @@ public class AssayService extends BaseService<AssayDomain> {
 
 		List<SlimAssayDomain> results = new ArrayList<SlimAssayDomain>();
 		
-		String cmd = "\nselect distinct g._assay_key, m._marker_key, m.symbol, g._assaytype_key" + 
-				"\nfrom bib_citation_cache aa, gxd_assay g, mrk_marker m" + 
+		String cmd = "\nselect distinct g._assay_key, m._marker_key, m.symbol, g._assaytype_key, ag.accid" + 
+				"\nfrom bib_citation_cache aa, gxd_assay g, mrk_marker m, acc_accession ag" + 
 				"\nwhere aa.jnumid = '" + jnumid + "'" +
 				"\nand aa._refs_key = g._refs_key" +
 				"\nand m._marker_key = g._marker_key" +
-				"\norder by m.symbol, g._assaytype_key, b.short_citation";
+				"\norder by m.symbol, g._assaytype_key, b.short_citation, ag.accid";
 		
 		log.info(cmd);	
 		

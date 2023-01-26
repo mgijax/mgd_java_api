a76182ed1c05daf718b0347da858f005845dfa01
Lori Corbani <lori.corbani@jax.org>
2023-01-26 13:17:32 -0500

    fl2b/allele-to-driver

--- src/main/java/org/jax/mgi/mgd/api/model/voc/service/VocabService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/voc/service/VocabService.java
@@ -441,7 +441,7 @@ public class VocabService extends BaseService<VocabularyDomain> {
 		}
 		else if (vocabName.equals("driverComponents")) {		
 			cmd = "select _term_key as termKey, term from voc_term where _vocab_key = " + vocabKey
-					+ "\nand term in (\"driver_components\")"
+					+ "\nand term in ('driver_components')"
 					+ "\norder by term";
 		}		
 		else if (vocabName.equals("properties")) {		

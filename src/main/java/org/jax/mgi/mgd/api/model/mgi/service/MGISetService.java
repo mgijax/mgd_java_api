bcc16c169c400d7d8188bf34a4a4f0ee9935383e
Lori Corbani <lori.corbani@jax.org>
2023-02-17 15:58:40 -0500

    wts2-888/fl2-221/API revamp: EMAPA

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/service/MGISetService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/service/MGISetService.java
@@ -64,9 +64,15 @@ public class MGISetService extends BaseService<MGISetDomain> {
 		// process celltype clipboard set member
 		else if(domain.getCelltypeClipboardMembers() != null ) {
 			if (setMemberService.process(domain.getSetKey(),  domain.getCelltypeClipboardMembers(), user)) {
-				log.info("Called setMemberService");
+				log.info("Called getCelltypeClipboard setMemberService");
 				modified = true;
 			}
+		}
+		// process emapa clipboard set member
+		else if(domain.getEmapaClipboardMembers() != null ) {
+			if (setMemberService.process(domain.getSetKey(),  domain.getEmapaClipboardMembers(), user)) {
+				modified = true;
+			}	
 		}
 		
 		// only if modifications were actually made
@@ -153,6 +159,10 @@ public class MGISetService extends BaseService<MGISetDomain> {
 		else if (domain.getSetKey().equals("1059")) {
 			domain.setCelltypeClipboardMembers(listOfMembers);
 		}
+		else if (domain.getSetKey().equals("1046")) {
+			domain.setEmapaClipboardMembers(listOfMembers);
+		}	
+
 		results.add(domain);
 		return results;
 	}
@@ -197,6 +207,10 @@ public class MGISetService extends BaseService<MGISetDomain> {
 		else if (domain.getSetKey().equals("1059")) {
 			domain.setCelltypeClipboardMembers(listOfMembers);
 		}
+		else if (domain.getSetKey().equals("1046")) {
+			domain.setEmapaClipboardMembers(listOfMembers);
+		}
+		
 		results.add(domain);
 		return results;
 	}

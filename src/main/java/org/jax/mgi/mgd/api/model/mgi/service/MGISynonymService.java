e7ddcc0914325ee485f37148c73c46be5fbd9a06
Lori Corbani <lori.corbani@jax.org>
2023-03-16 11:45:46 -0400

    wts2-1134/fl2-244/Auto-incorrect function in Synonym field in Allele Module

--- src/main/java/org/jax/mgi/mgd/api/model/mgi/service/MGISynonymService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/mgi/service/MGISynonymService.java
@@ -153,6 +153,8 @@ public class MGISynonymService extends BaseService<MGISynonymDomain> {
 					refsKey = "null";
 				}
 				
+			        domain.get(i).setSynonym(domain.get(i).getSynonym().replaceAll("'", "''"));
+
 				cmd = "select count(*) from MGI_insertSynonym ("
 							+ user.get_user_key().intValue()
 							+ "," + parentKey
@@ -185,7 +187,6 @@ public class MGISynonymService extends BaseService<MGISynonymDomain> {
 				MGISynonym entity = synonymDAO.get(Integer.valueOf(domain.get(i).getSynonymKey()));
 		
 				entity.setSynonymType(synonymTypeDAO.get(Integer.valueOf(domain.get(i).getSynonymTypeKey())));
-			        domain.get(i).setSynonym(domain.get(i).getSynonym().replaceAll("'", "''"));
 				entity.setSynonym(domain.get(i).getSynonym());
 				
 				// reference can be null

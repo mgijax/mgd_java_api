3545d23aba3a78c761cb8f85b8483c77ec6dcc7b
Lori Corbani <lori.corbani@jax.org>
2023-04-18 12:29:58 -0400

    commented out the executeProt logging

--- src/main/java/org/jax/mgi/mgd/api/util/SQLExecutor.java
+++ src/main/java/org/jax/mgi/mgd/api/util/SQLExecutor.java
@@ -133,11 +133,11 @@ public class SQLExecutor {
 			if (cursorLimit > 0) {
 				stmt.setFetchSize(cursorLimit);
 			}
-			start = new Date();
-			log.info("start executeQuery:" + start);
+			//start = new Date();
+			//log.info("start executeQuery:" + start);
 			set = stmt.executeQuery(query);
-			end = new Date();
-			log.info("end executeQuery:" + end);			
+			//end = new Date();
+			//log.info("end executeQuery:" + end);			
 			return set;
 		} catch (Exception e) {
 			e.printStackTrace();

b38770f287dafa9aa5bacbf24d1b2f0297db0742
Lori Corbani <lori.corbani@jax.org>
2024-02-08 12:06:50 -0500

    wts2-1400/fl2-739/Smart sort in Allele and Variant modules search results

--- src/main/java/org/jax/mgi/mgd/api/model/all/service/AlleleVariantService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/service/AlleleVariantService.java
@@ -327,11 +327,11 @@ public class AlleleVariantService extends BaseService<AlleleVariantDomain> {
 
 		// building SQL command : select + from + where + orderBy
 		String cmd = "";		
-		String select = "select distinct v._variant_key, a._allele_key, a.symbol";
+		String select = "select distinct v._variant_key, a._allele_key, a.symbol, left(a.symbol, 1), substring(a.symbol, '\\\\d+')::int";
 		String from = "from all_variant v, all_allele a";
 		String where = "where v._sourcevariant_key is not null"
 				+ "\nand v._allele_key = a._allele_key";
-		String orderBy = "order by a.symbol";
+		String orderBy = "order by left(a.symbol, 1), substring(a.symbol, '\\d+')::int NULLS FIRST, a.symbol";
 		String limit = Constants.SEARCH_RETURN_LIMIT;
 		String value;	
 		Boolean from_strain = false;

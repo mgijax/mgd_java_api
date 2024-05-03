7ac9a8abb58a9115d6ea34123ee48d5c562cb7f1
Lori Corbani <lori.corbani@jax.org>
2024-05-03 14:11:12 -0400

    wts2-1410/fl2-853/Allele search result sorting tweak

--- src/main/java/org/jax/mgi/mgd/api/model/all/domain/AlleleDomain.java
+++ src/main/java/org/jax/mgi/mgd/api/model/all/domain/AlleleDomain.java
@@ -64,7 +64,10 @@ public class AlleleDomain extends BaseDomain {
 	private String creation_date;
 	private String modification_date;
 	private String approval_date;
-		
+	
+	// order by
+	private String orderBy = "1";
+	
 	// molecular reference needed for driverGenes
 	private String molRefKey;
 	

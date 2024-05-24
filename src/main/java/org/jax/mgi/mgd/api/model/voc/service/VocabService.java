431ae77b716a50c388298f78ef0586739ef7d180
Lori Corbani <lori.corbani@jax.org>
2024-05-24 09:36:23 -0400

    wts2-1478/GXD assays (blots): tweak ordering of Strength of Staining drop down

--- src/main/java/org/jax/mgi/mgd/api/model/voc/service/VocabService.java
+++ src/main/java/org/jax/mgi/mgd/api/model/voc/service/VocabService.java
@@ -382,10 +382,13 @@ public class VocabService extends BaseService<VocabularyDomain> {
 					"\nand v.term = 'Present'" + 
 				"\nunion" + 
 				"\nselect v._term_key as termKey, v.term as term, v.abbreviation, 2 as orderBy from voc_term v where v._vocab_key = " + vocabKey +
+					"\nand v.term = 'Absent'" + 					
+				"\nunion" + 
+				"\nselect v._term_key as termKey, v.term as term, v.abbreviation, 3 as orderBy from voc_term v where v._vocab_key = " + vocabKey +
 					"\nand v.term = 'Not Specified'" + 
 				"\nunion" +					
-				"\nselect v._term_key as termKey, v.term as term, v.abbreviation, 3 as orderBy from voc_term v where v._vocab_key = " + vocabKey +
-					"\nand v.term not in ('Not Specified', 'Present')" + 
+				"\nselect v._term_key as termKey, v.term as term, v.abbreviation, 4 as orderBy from voc_term v where v._vocab_key = " + vocabKey +
+					"\nand v.term not in ('Not Specified', 'Present', 'Absent')" + 
 				"\norder by orderBy, term\n";
 		}		
 		else {
